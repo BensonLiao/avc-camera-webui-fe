b45588d096b90034672efc7d359725c9d391ab58
Kelp <kelp@phate.org>
2019-06-14 17:33:08 +0800

    Added comments.

--- src/core/apis/web-api.js
+++ src/core/apis/web-api.js
@@ -3,6 +3,19 @@ const api = require('./');
 module.exports = {
   account: {
     login: ({account, password}) => api({
+      /*
+      Do authentication with account and password.
+      @param args {Object}
+        account: {String}
+        password: {String}
+      @returns {Promise<Object>}
+      @response 200 {UserModel}
+      @response 400 {Object}
+        extra: {Object}
+          loginFailedTimes: {Number}
+          isTooManyLoginFailed: {Boolean}
+          loginLockExpiredTime: {Date}
+       */
       method: 'post',
       url: '/api/account/_login',
       data: {account, password}
