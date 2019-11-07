c67cc30a1948c2d984f35acb16d28353cba4b04c
Kelp <kelp@phate.org>
2019-11-07 16:34:00 +0800

    Fixed eslint.

--- src/web/pages/members/members.js
+++ src/web/pages/members/members.js
@@ -70,7 +70,7 @@ module.exports = class Members extends Base {
       })
       .catch(error => {
         progress.done();
-        utils.renderError(error)
+        utils.renderError(error);
       });
   };
 
