364ebf551b5bf31f3ea1938c838bba53da001316
BensonLiao <Benson.Liao@androvideo.com>
2019-11-15 18:32:26 +0800

    Add uuid for post request, remove id property deletion

--- src/core/apis/index.dev.js
+++ src/core/apis/index.dev.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+const uuidv4 = require('uuid/v4');
 const mockDB = require('../db-mock');
 mockDB.init();
 const db = mockDB.get();
-const MockAdapter = require('axios-mock-adapter');
 const mockAxios = new MockAdapter(axios);
 mockAxios.onGet('/api/groups').reply(() => {
   return [200, {
@@ -23,10 +24,8 @@ mockAxios.onGet('/api/groups').reply(() => {
   })
   .onPost('/api/groups').reply(config => {
     const newItem = JSON.parse(config.data);
-    const newId = '40d1e5fd-3dd7-4ad1-a4c8-0ca928060780';
-    newItem.id = newId;
+    newItem.id = uuidv4();
     db.get('groups').push(newItem).write();
-    delete newItem[newId];
     return [200, newItem];
   })
   .onDelete(/api\/groups\/[a-f0-9-]{36}$/).reply(config => {
@@ -57,10 +56,8 @@ mockAxios.onGet('/api/groups').reply(() => {
   })
   .onPost('/api/members').reply(config => {
     const newItem = JSON.parse(config.data);
-    const newId = '40d1e5fd-3dd7-4ad1-a4c8-0ca928060788';
-    newItem.id = newId;
+    newItem.id = uuidv4();
     db.get('members').push(newItem).write();
-    delete newItem[newId];
     return [200, newItem];
   })
   .onDelete(/api\/members\/[a-f0-9-]{36}$/).reply(config => {
