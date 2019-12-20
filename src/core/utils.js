8107773901ef4a6d0b3f361257154fa91ea56436
BensonLiao <Benson.Liao@androvideo.com>
2019-12-20 16:48:45 +0800

    Refactor all format in utils

--- src/core/utils.js
+++ src/core/utils.js
@@ -3,11 +3,12 @@ const {getRouter} = require('capybara-router');
 const dayjs = require('dayjs');
 const _ = require('../languages');
 
+/**
+ * Format time range.
+ * @param {Array<Number>} timeRange
+ * @returns {String}
+ */
 exports.formatTimeRange = timeRange => {
-  /*
-  @param timeRange {Array<Number>}
-  @returns {String}
-   */
   const startHour = `${Math.floor(timeRange[0])}`.padStart(2, '0');
   const startMinute = timeRange[0] % 1 === 0 ? '00' : '30';
   const endHour = `${Math.floor(timeRange[1])}`.padStart(2, '0');
@@ -15,16 +16,16 @@ exports.formatTimeRange = timeRange => {
   return `${startHour}:${startMinute} - ${endHour}:${endMinute}`;
 };
 
+/**
+ * Format date.
+ * @param {String|Date} date
+ * @param {Object} options
+ * @property {Boolean} withSecond
+ * @property {Boolean} withoutTime
+ * @property {String} format
+ * @returns {String} e.g. "2019/10/2 20:59"
+ */
 exports.formatDate = (date, {withSecond, withoutTime, format} = {}) => {
-  /*
-  Format date.
-  @param date {String|Date}
-  @param options {Object}
-    withSecond: {Boolean}
-    withoutTime: {Boolean}
-    format {String}
-  @returns {String} eg: "2019/10/2 20:59"
-   */
   if (!date) {
     return '';
   }
@@ -42,12 +43,12 @@ exports.formatDate = (date, {withSecond, withoutTime, format} = {}) => {
   return `${dayjs(date).format(formats[0])} ${dayjs(date).format(formats[1])}`;
 };
 
+/**
+ * Format number. e.g. 1000 -> 1,000
+ * @param {Number|String|null} value
+ * @returns {String}
+ */
 exports.formatNumber = value => {
-  /*
-  1000 -> 1,000
-  @param value {Number|String|null}
-  @returns {String}
-   */
   if (value == null) {
     return '';
   }
@@ -55,22 +56,25 @@ exports.formatNumber = value => {
   return `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
+/**
+ * Set the default language via cookie.
+ * This function isn't write to the camera.
+ * @param {String} languageCode
+ * @returns {void}
+ */
 exports.setDefaultLanguage = languageCode => {
-  /*
-  Set the default language via cookie.
-  This function isn't write to the camera.
-  @param languageCode {String}
-   */
   Cookies.set(window.config.cookies.language, languageCode, {expires: 30});
 };
 
+/**
+ * Convert the fastest-validator validator for Formik.
+ * @param {Function} validator
+ * @param {Array<String>} passwordFields Make sure the confirm password and the password are equal.
+ * @returns {Function} inner function `validator`.
+ * @param {Object} values field value to be validate.
+ * @returns {Object} validate results.
+ */
 exports.makeFormikValidator = (validator, passwordFields) => values => {
-  /*
-  Convert the fastest-validator validator for Formik.
-  @param validator {Function}
-  @param passwordFields {Array<String>} Make sure the confirm password and the password are equal.
-  @returns {Function} (values) =>
-   */
   const result = {};
   const checkResult = validator(values);
 
@@ -97,16 +101,18 @@ exports.renderError = error => {
   throw error;
 };
 
+/**
+ * Log mock XHR like axios with console.groupCollapsed() and return mock response.
+ * @param {Object} req XHR request instance, or if we use library like axios then `req` is the axios request config and contains things like `url`.
+ * @see https://github.com/axios/axios#request-config
+ * @param {Array<[Number, ?Object, ?Object]>|Object} res Accept any type of response, or if we use library like axios-mock-adapter then this will be an array in the form of [status, data, headers].
+ * @see https://github.com/ctimmerm/axios-mock-adapter
+ * @returns {Array<[Number, ?Object, ?Object]>} Same object as `res`.
+ */
 exports.mockResponseWithLog = (req, res) => {
-  /*
-  Log mock XHR like axios with console.groupCollapsed() and return mock response.
-  @param req {Object} XHR request instance, or if we use library like axios then `config` is the axios config and contains things like the url. ref: https://github.com/axios/axios#request-config
-  @param res {*} Accept any type of response, or if we use library like axios-mock-adapter then this will be an array in the form of [status, data, headers]. ref: https://github.com/ctimmerm/axios-mock-adapter
-  @returns {*} Same as @param res
-   */
   console.groupCollapsed('mock axios xhr log:');
-  console.log('req config:', req);
-  console.log('res: [status, data, headers]', res);
+  console.log('request config:', req);
+  console.log('response: [status, data, headers]', res);
   console.groupEnd();
   return res;
 };
