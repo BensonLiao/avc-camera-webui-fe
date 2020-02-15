178cca74dd94fc3a844c1684259e633f9886471e
BensonLiao <Benson.Liao@androvideo.com>
2020-02-15 18:33:35 +0800

    Update active class of left navbar

--- src/web/pages/layout.js
+++ src/web/pages/layout.js
@@ -142,9 +142,9 @@ module.exports = class Layout extends Base {
             'web.users.members.modify-group',
             'web.users.members.new-member',
             'web.users.members.details',
-            'web.users.account',
-            'web.users.account.details',
-            'web.users.account.new-user',
+            'web.users.accounts',
+            'web.users.accounts.details',
+            'web.users.accounts.new-user',
             'web.users.events'
           ].indexOf(this.state.currentRouteName) >= 0
         }
