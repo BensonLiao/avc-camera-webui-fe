e5e42b3eb1428bcb6bc061e1cbb5562dbaf73378
BensonLiao <Benson.Liao@androvideo.com>
2020-03-05 18:09:55 +0800

    Remove error oops icon

--- src/web/pages/shared/error-page.js
+++ src/web/pages/shared/error-page.js
@@ -3,7 +3,6 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const imageCode404 = require('../../../resource/error-404-code.svg');
 const imageCode500 = require('../../../resource/error-500-code.svg');
-const imageOops = require('../../../resource/error-oops.svg');
 const _ = require('../../../languages');
 
 module.exports = class ErrorPage extends React.Component {
@@ -41,7 +40,7 @@ module.exports = class ErrorPage extends React.Component {
               <img src={this.state.status === 404 ? imageCode404 : imageCode500}/>
             </div>
             <div className="col-6 message-container">
-              <img src={imageOops}/> <span className="message-title">{messageTitle}</span>
+              <span className="message-title">{messageTitle}</span>
               <p className="message-subtitle">{messageSubtitle}</p>
               <a className="btn btn-primary text-light rounded-pill" href="/">
                 {_('Go to Home')} <i className="fas fa-arrow-right fa-fw"/>
