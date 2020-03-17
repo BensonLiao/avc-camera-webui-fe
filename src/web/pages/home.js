6fee470a5fb89e5f684cbce3e8842c091d9f9faa
BensonLiao <Benson.Liao@androvideo.com>
2020-03-17 19:35:13 +0800

    Copy all component function from media/stream

--- src/web/pages/home.js
+++ src/web/pages/home.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const download = require('downloadjs');
 const classNames = require('classnames');
+const {getRouter} = require('capybara-router');
 const PropTypes = require('prop-types');
 const React = require('react');
 const progress = require('nprogress');
@@ -14,8 +15,14 @@ const _ = require('../../languages');
 const utils = require('../../core/utils');
 const api = require('../../core/apis/web-api');
 const Slider = require('../../core/components/fields/slider');
+const Dropdown = require('../../core/components/fields/dropdown');
 const FormikEffect = require('../../core/components/formik-effect');
 const deviceNameValidator = require('../validations/system/device-name-validator');
+const StreamSettingsSchema = require('webserver-form-schema/stream-settings-schema');
+const StreamFormat = require('webserver-form-schema/constants/stream-format');
+const StreamResolution = require('webserver-form-schema/constants/stream-resolution');
+const StreamBandwidthManagement = require('webserver-form-schema/constants/stream-bandwidth-management');
+const StreamGOV = require('webserver-form-schema/constants/stream-gov');
 
 module.exports = class Home extends Base {
   static get propTypes() {
@@ -271,6 +278,208 @@ module.exports = class Home extends Base {
             {_('Reset to Default')}
           </button>
         </div>
+      </Form>
+    );
+  };
+
+  generateOnChangeBandwidthManagement = bandwidthManagement => {
+    return event => {
+      event.preventDefault();
+      this.setState({bandwidthManagement});
+    };
+  }
+
+  onSubmit = values => {
+    progress.start();
+    api.multimedia.updateStreamSettings(values)
+      .then(getRouter().reload)
+      .catch(error => {
+        progress.done();
+        utils.renderError(error);
+      });
+  };
+
+  onClickResetButton = event => {
+    event.preventDefault();
+    progress.start();
+    api.multimedia.resetStreamSettings()
+      .then(getRouter().reload)
+      .catch(utils.renderError)
+      .catch(error => {
+        progress.done();
+        utils.renderError(error);
+      });
+  };
+
+  fieldsRender = (fieldNamePrefix, options, values) => {
+    return (
+      <>
+        <div className="form-group">
+          <label>{_('Format')}</label>
+          <div className="select-wrapper border rounded-pill overflow-hidden">
+            <Field
+              name={`${fieldNamePrefix}.format`}
+              component="select"
+              className="form-control border-0"
+            >
+              {
+                options.format.map(option => (
+                  <option key={option.value} value={option.value}>{option.label}</option>
+                ))
+              }
+            </Field>
+          </div>
+        </div>
+        <div className="form-group">
+          <label>{_('Resolution')}</label>
+          <div className="select-wrapper border rounded-pill overflow-hidden">
+            <Field
+              name={`${fieldNamePrefix}.resolution`}
+              component="select"
+              className="form-control border-0"
+            >
+              {
+                options.resolution.map(option => (
+                  <option key={option.value} value={option.value}>{option.label}</option>
+                ))
+              }
+            </Field>
+          </div>
+        </div>
+        <div className="form-group">
+          <label>{_('Frame Rate (FPS)')}</label>
+          <div className="select-wrapper border rounded-pill overflow-hidden">
+            <Field
+              name={`${fieldNamePrefix}.frameRate`}
+              component="select"
+              className="form-control border-0"
+            >
+              {
+                options.frameRate.map(option => (
+                  <option key={option.value} value={option.value}>{option.label}</option>
+                ))
+              }
+            </Field>
+          </div>
+        </div>
+        <div className="form-group">
+          <label>{_('Bandwidth Management')}</label>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <Field
+                name={`${fieldNamePrefix}.bandwidthManagement`}
+                component={Dropdown}
+                buttonClassName="btn btn-outline-primary rounded-left"
+                menuClassName="dropdown-menu-right"
+                items={options.bandwidthManagement.map(x => ({value: x.value, label: x.label}))}
+              />
+            </div>
+            <Field type="text" name={`${fieldNamePrefix}.bitRate`} className={classNames('form-control dynamic', {show: values.bandwidthManagement === StreamBandwidthManagement.mbr})}/>
+            <input readOnly type="text" className={classNames('form-control dynamic', {show: values.bandwidthManagement === StreamBandwidthManagement.vbr})} placeholder="Auto"/>
+            <Field type="text" name={`${fieldNamePrefix}.bitRate`} className={classNames('form-control dynamic', {show: values.bandwidthManagement === StreamBandwidthManagement.cbr})}/>
+            <div className="input-group-append">
+              <span className="input-group-text">Kbps</span>
+            </div>
+          </div>
+        </div>
+        <div className="form-group">
+          <label>{_('GOV')}</label>
+          <div className="select-wrapper border rounded-pill overflow-hidden">
+            <Field
+              name={`${fieldNamePrefix}.gov`}
+              component="select"
+              className="form-control border-0"
+            >
+              {
+                options.gov.map(option => (
+                  <option key={option.value} value={option.value}>{option.label}</option>
+                ))
+              }
+            </Field>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  formRender = ({values}) => {
+    const channelAOptions = {
+      format: StreamFormat.all().map(x => ({label: x, value: x})),
+      resolution: StreamResolution.all().filter(x => Number(x) <= 8 && Number(x) !== 4).map(x => ({label: _(`stream-resolution-${x}`), value: x})),
+      frameRate: (() => {
+        const result = [];
+        for (let index = StreamSettingsSchema.channelA.props.frameRate.min; index <= StreamSettingsSchema.channelA.props.frameRate.max; index += 1) {
+          result.push({label: `${index}`, value: `${index}`});
+        }
+
+        return result;
+      })(),
+      bandwidthManagement: StreamBandwidthManagement.all().map(x => ({label: _(`stream-bandwidth-management-${x}`), value: x})),
+      gov: StreamGOV.all().map(x => ({label: x, value: x}))
+    };
+    const channelBOptions = {
+      format: StreamFormat.all().map(x => ({label: x, value: x})),
+      resolution: (() => {
+        let options;
+        if (Number(values.channelA.resolution) <= Number(StreamResolution['4'])) {
+          options = [
+            StreamResolution['0'],
+            StreamResolution['1'],
+            StreamResolution['2'],
+            StreamResolution['3'],
+            StreamResolution['4']
+          ];
+        } else {
+          options = [
+            StreamResolution['5'],
+            StreamResolution['6'],
+            StreamResolution['7'],
+            StreamResolution['8']
+          ];
+        }
+
+        return options.map(x => ({label: _(`stream-resolution-${x}`), value: x}));
+      })(),
+      frameRate: (() => {
+        const result = [];
+        for (let index = StreamSettingsSchema.channelB.props.frameRate.min; index <= StreamSettingsSchema.channelB.props.frameRate.max; index += 1) {
+          result.push({label: `${index}`, value: `${index}`});
+        }
+
+        return result;
+      })(),
+      bandwidthManagement: StreamBandwidthManagement.all().map(x => ({label: _(`stream-bandwidth-management-${x}`), value: x})),
+      gov: StreamGOV.all().map(x => ({label: x, value: x}))
+    };
+
+    return (
+      <Form className="card-body">
+        <div className="tab-content">
+          <div className="tab-pane fade show active" id="tab-channel-a">
+            {this.fieldsRender('channelA', channelAOptions, values.channelA)}
+          </div>
+          <div className="tab-pane fade" id="tab-channel-b">
+            {this.fieldsRender('channelB', channelBOptions, values.channelB)}
+          </div>
+        </div>
+
+        <div className="form-group mt-5">
+          <button
+            type="submit"
+            className="btn btn-block btn-primary rounded-pill"
+            disabled={this.state.$isApiProcessing}
+          >
+            {_('Apply')}
+          </button>
+        </div>
+        <button
+          type="button"
+          className="btn btn-block btn-outline-primary rounded-pill"
+          disabled={this.state.$isApiProcessing}
+          onClick={this.onClickResetButton}
+        >
+          {_('Reset to Default')}
+        </button>
       </Form>
     );
   };
