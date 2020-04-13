61a46655f3251607e128d078188b5f2f05299545
Rodger <rodger.lo@androvideo.com>
2020-04-13 15:12:34 +0800

    home page stream setting refactored as component

--- src/web/pages/home.js
+++ src/web/pages/home.js
@@ -15,15 +15,10 @@ const _ = require('../../languages');
 const utils = require('../../core/utils');
 const api = require('../../core/apis/web-api');
 const Slider = require('../../core/components/fields/slider');
-const Dropdown = require('../../core/components/fields/dropdown');
 const FormikEffect = require('../../core/components/formik-effect');
 const deviceNameValidator = require('../validations/system/device-name-validator');
-const StreamSettingsSchema = require('webserver-form-schema/stream-settings-schema');
-const StreamFormat = require('webserver-form-schema/constants/stream-format');
-const StreamResolution = require('webserver-form-schema/constants/stream-resolution');
-const StreamBandwidthManagement = require('webserver-form-schema/constants/stream-bandwidth-management');
-const StreamGOV = require('webserver-form-schema/constants/stream-gov');
 const {AVAILABLE_LANGUAGE_CODES, DEVICE_NAME_CHAR_MAX} = require('../../core/constants');
+const StreamSetting = require('./media/stream-setting');
 
 module.exports = class Home extends Base {
   static get propTypes() {
@@ -311,224 +306,12 @@ module.exports = class Home extends Base {
     );
   };
 
-  fieldsRender = (fieldNamePrefix, options, values) => {
-    return (
-      <>
-        <div className="form-group">
-          <label>{_('Format')}</label>
-          <div className="select-wrapper border rounded-pill overflow-hidden">
-            <Field
-              readOnly
-              name={`${fieldNamePrefix}.format`}
-              component="select"
-              className="form-control border-0 select-readonly"
-            >
-              {
-                options.format.map(option => (
-                  <option key={option.value} value={option.value}>{option.label}</option>
-                ))
-              }
-            </Field>
-          </div>
-        </div>
-        <div className="form-group">
-          <label>{_('Resolution')}</label>
-          <div className="select-wrapper border rounded-pill overflow-hidden">
-            <Field
-              readOnly
-              name={`${fieldNamePrefix}.resolution`}
-              component="select"
-              className="form-control border-0 select-readonly"
-            >
-              {
-                options.resolution.map(option => (
-                  <option key={option.value} value={option.value}>{option.label}</option>
-                ))
-              }
-            </Field>
-          </div>
-        </div>
-        <div className="form-group">
-          <label>{_('Frame Rate (FPS)')}</label>
-          <div className="select-wrapper border rounded-pill overflow-hidden">
-            <Field
-              name={`${fieldNamePrefix}.frameRate`}
-              component="select"
-              className="form-control border-0"
-            >
-              {
-                options.frameRate.map(option => (
-                  <option key={option.value} value={option.value}>{option.label}</option>
-                ))
-              }
-            </Field>
-          </div>
-        </div>
-        <div className="form-group">
-          <label>{_('Bandwidth Management')}</label>
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <Field
-                name={`${fieldNamePrefix}.bandwidthManagement`}
-                component={Dropdown}
-                buttonClassName="btn btn-outline-primary rounded-left"
-                menuClassName="dropdown-menu-right"
-                items={options.bandwidthManagement.map(x => ({value: x.value, label: x.label}))}
-              />
-            </div>
-            <Field type="text" name={`${fieldNamePrefix}.bitRate`} className={classNames('form-control dynamic', {show: values.bandwidthManagement === StreamBandwidthManagement.mbr})}/>
-            <input readOnly type="text" className={classNames('form-control dynamic', {show: values.bandwidthManagement === StreamBandwidthManagement.vbr})} placeholder="Auto"/>
-            <Field type="text" name={`${fieldNamePrefix}.bitRate`} className={classNames('form-control dynamic', {show: values.bandwidthManagement === StreamBandwidthManagement.cbr})}/>
-            <div className="input-group-append">
-              <span className="input-group-text">Kbps</span>
-            </div>
-          </div>
-          <small className="text-info mb-3">
-            {_('{0} - {1} Kbps', [StreamSettingsSchema.channelA.props.bitRate.min, StreamSettingsSchema.channelA.props.bitRate.max])}
-          </small>
-        </div>
-        <div className="form-group">
-          <label>{_('GOV')}</label>
-          <div className="select-wrapper border rounded-pill overflow-hidden">
-            <Field
-              readOnly
-              name={`${fieldNamePrefix}.gov`}
-              component="select"
-              className="form-control border-0 select-readonly"
-            >
-              {
-                options.gov.map(option => (
-                  <option key={option.value} value={option.value}>{option.label}</option>
-                ))
-              }
-            </Field>
-          </div>
-        </div>
-      </>
-    );
-  };
-
-  formRender = ({values}) => {
-    const channelAOptions = {
-      format: StreamFormat.all().map(x => ({label: x, value: x})),
-      resolution: StreamResolution.all().filter(x => Number(x) <= 8 && Number(x) !== 4).map(x => ({label: _(`stream-resolution-${x}`), value: x})),
-      frameRate: (() => {
-        const result = [];
-        for (let index = StreamSettingsSchema.channelA.props.frameRate.min; index <= StreamSettingsSchema.channelA.props.frameRate.max; index += 1) {
-          result.push({label: `${index}`, value: `${index}`});
-        }
-
-        return result;
-      })(),
-      bandwidthManagement: StreamBandwidthManagement.all().map(x => ({label: _(`stream-bandwidth-management-${x}`), value: x})),
-      gov: StreamGOV.all().map(x => ({label: x, value: x}))
-    };
-    const channelBOptions = {
-      format: StreamFormat.all().map(x => ({label: x, value: x})),
-      resolution: (() => {
-        let options;
-        if (Number(values.channelA.resolution) <= Number(StreamResolution['4'])) {
-          options = [
-            StreamResolution['0'],
-            StreamResolution['1'],
-            StreamResolution['2'],
-            StreamResolution['3'],
-            StreamResolution['4']
-          ];
-        } else {
-          options = [
-            StreamResolution['5'],
-            StreamResolution['6'],
-            StreamResolution['7'],
-            StreamResolution['8']
-          ];
-        }
-
-        return options.map(x => ({label: _(`stream-resolution-${x}`), value: x}));
-      })(),
-      frameRate: (() => {
-        const result = [];
-        for (let index = StreamSettingsSchema.channelB.props.frameRate.min; index <= StreamSettingsSchema.channelB.props.frameRate.max; index += 1) {
-          result.push({label: `${index}`, value: `${index}`});
-        }
-
-        return result;
-      })(),
-      bandwidthManagement: StreamBandwidthManagement.all().map(x => ({label: _(`stream-bandwidth-management-${x}`), value: x})),
-      gov: StreamGOV.all().map(x => ({label: x, value: x}))
-    };
-
-    return (
-      <>
-        <div className="card-header d-flex align-items-center justify-content-between rounded-0">
-          {_('Stream Settings')}
-          <button
-            type="button"
-            className="btn btn-outline-light rounded-pill"
-            disabled={this.state.$isApiProcessing}
-            onClick={this.onClickResetButton}
-          >
-            {_('Reset to Default')}
-          </button>
-        </div>
-        <nav>
-          <div className="nav nav-tabs">
-            <a
-              className="nav-item nav-link active"
-              data-toggle="tab"
-              href="#tab-channel-a"
-            >
-              {_('Stream {0}', '01')}
-            </a>
-            <a
-              className="nav-item nav-link"
-              data-toggle="tab"
-              href="#tab-channel-b"
-            >
-              {_('Stream {0}', '02')}
-            </a>
-          </div>
-        </nav>
-        <Form className="card-body">
-          <div className="tab-content">
-            <div className="tab-pane fade show active" id="tab-channel-a">
-              {this.fieldsRender('channelA', channelAOptions, values.channelA)}
-            </div>
-            <div className="tab-pane fade" id="tab-channel-b">
-              {this.fieldsRender('channelB', channelBOptions, values.channelB)}
-            </div>
-          </div>
-
-          <div className="form-group mt-5">
-            <button
-              type="submit"
-              className="btn btn-block btn-primary rounded-pill"
-              disabled={this.state.$isApiProcessing}
-            >
-              {_('Apply')}
-            </button>
-          </div>
-        </Form>
-      </>
-    );
-  };
-
   generateOnChangeBandwidthManagement = bandwidthManagement => {
     return event => {
       event.preventDefault();
       this.setState({bandwidthManagement});
     };
   }
-
-  onSubmit = values => {
-    progress.start();
-    api.multimedia.updateStreamSettings(values)
-      .then(getRouter().reload)
-      .catch(error => {
-        progress.done();
-        utils.renderError(error);
-      });
-  };
 
   onClickResetButton = event => {
     event.preventDefault();
@@ -689,12 +472,9 @@ module.exports = class Home extends Base {
                   <Formik initialValues={this.generateInitialValues(this.props.videoSettings)}>
                     {this.videoSettingsFormRender}
                   </Formik>
-                  <Formik
-                    initialValues={streamSettings}
-                    onSubmit={this.onSubmit}
-                  >
-                    {this.formRender}
-                  </Formik>
+                  <StreamSetting
+                    homePage
+                    streamSettings={streamSettings}/>
                 </div>
               </div>
             </div>
