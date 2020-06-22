fe986c46273ba1aac8ff88d61a6e9b0b6faae053
Rodger <rodger.lo@androvideo.com>
2020-06-22 14:44:13 +0800

    refactor form subject

--- src/web/pages/notification/cards-form.js
+++ src/web/pages/notification/cards-form.js
@@ -12,10 +12,11 @@ const NotificationFaceRecognitionCondition = require('webserver-form-schema/cons
 const NotificationFaceRecognitionVMSEvent = require('webserver-form-schema/constants/notification-face-recognition-vms-event');
 const _ = require('../../../languages');
 const CustomTooltip = require('../../../core/components/tooltip');
-const {NOTIFY_CARDS_MAX, NOTIFY_CARDS_EMAIL_MAX} = require('../../../core/constants');
+const {NOTIFY_CARDS_MAX} = require('../../../core/constants');
 const utils = require('../../../core/utils');
 const CardsFormSchedule = require('./cards-form-schedule');
 const CardsFormRule = require('./cards-form-rule');
+const CardsFormSubject = require('./cards-form-subject');
 
 module.exports = class CardsForm extends React.PureComponent {
   static get propTypes() {
@@ -110,41 +111,6 @@ module.exports = class CardsForm extends React.PureComponent {
     const onClickTitleEditButton = event => {
       event.preventDefault();
       this.cardFormTitleRef.current.focus();
-    };
-
-    const onClickAddEmail = event => {
-      event.preventDefault();
-      validateField('$email').then(value => {
-        if (!value) {
-          const emails = [...values.emails];
-          emails.push(values.$email);
-          setFieldValue('emails', emails);
-          setFieldValue('$email', '');
-        }
-      });
-    };
-
-    const validateEmail = () => {
-      if (values.$email) {
-        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.$email)) {
-          return _('Invalid email address');
-        }
-
-        const emails = [...values.emails];
-
-        if (emails.length > NOTIFY_CARDS_EMAIL_MAX - 1) {
-          return _('Number of receiver E-mails limit is 64');
-        }
-
-        return utils.duplicateCheck(emails, values.$email, _('Duplicate E-mail found'));
-      }
-    };
-
-    const generateDeleteEmailHandler = index => event => {
-      event.preventDefault();
-      const emails = [...values.emails];
-      emails.splice(index, 1);
-      setFieldValue('emails', emails);
     };
 
     const onChangeTitle = event => {
@@ -250,184 +216,13 @@ module.exports = class CardsForm extends React.PureComponent {
             <Tab.Content>
               <Tab.Pane eventKey="tab-notification-target">
                 {/* Subject settings */}
-                {/* I/O Notification */}
-                <div className="form-group d-flex justify-content-between align-items-center">
-                  <label className="mb-0">I/O</label>
-                  <div className="custom-control custom-switch">
-                    <Field name="isEnableGPIO" type="checkbox" className="custom-control-input" id="switch-notification-target-io"/>
-                    <label className="custom-control-label" htmlFor="switch-notification-target-io">
-                      <span>{_('ON')}</span>
-                      <span>{_('OFF')}</span>
-                    </label>
-                  </div>
-                </div>
-                <div className={classNames('form-group', values.isEnableGPIO ? '' : 'd-none')}>
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="form-group d-flex justify-content-between align-items-center">
-                        <label className="mb-0">{_('Output {0}', ['1'])}</label>
-                        <div className="custom-control custom-switch">
-                          <Field name="isEnableGPIO1" type="checkbox" className="custom-control-input" id="switch-notification-target-output-1"/>
-                          <label className="custom-control-label" htmlFor="switch-notification-target-output-1">
-                            <span>{_('ON')}</span>
-                            <span>{_('OFF')}</span>
-                          </label>
-                        </div>
-                      </div>
-                      <div className="d-flex justify-content-between align-items-center">
-                        <label className="mb-0">{_('Output {0}', ['2'])}</label>
-                        <div className="custom-control custom-switch">
-                          <Field name="isEnableGPIO2" type="checkbox" className="custom-control-input" id="switch-notification-target-output-2"/>
-                          <label className="custom-control-label" htmlFor="switch-notification-target-output-2">
-                            <span>{_('ON')}</span>
-                            <span>{_('OFF')}</span>
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <hr/>
-
-                {/* VMS Notification */}
-                <div className={classNames('form-group', {'d-none': values.type === NotificationCardType.digitalInput})}>
-                  <div className="form-group d-flex justify-content-between align-items-center">
-                    <label className="mb-0">{_('Video Management System')}</label>
-                    <div className="custom-control custom-switch">
-                      <Field name="isEnableVMS" type="checkbox" className="custom-control-input" id="switch-notification-target-vms" checked={values.isEnableVMS}/>
-                      <label className="custom-control-label" htmlFor="switch-notification-target-vms">
-                        <span>{_('ON')}</span>
-                        <span>{_('OFF')}</span>
-                      </label>
-                    </div>
-                  </div>
-                  <div className={classNames('form-group', {'d-none': values.type === NotificationCardType.motionDetection})}>
-                    <div className="card">
-                      <div className="card-body">
-                        <div className="form-group">
-                          <label className="text-size-16 mb-0">{_('Method')}</label>
-                        </div>
-                        <div className="form-group">
-                          {
-                            NotificationFaceRecognitionVMSEvent.all().map(RecognitionVMSEvent => (
-                              <div key={RecognitionVMSEvent} className="form-check mb-3">
-                                <Field name="faceRecognitionVMSEvent" className="form-check-input" type="radio" id={`input-notification-vms-event-${RecognitionVMSEvent}`} value={RecognitionVMSEvent}/>
-                                <label className="form-check-label" htmlFor={`input-notification-vms-event-${RecognitionVMSEvent}`}>
-                                  {_(`notification-vms-event-${RecognitionVMSEvent}`)}
-                                </label>
-                              </div>
-                            ))
-                          }
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <hr/>
-                </div>
-
-                {/* E-mail Notification */}
-                <div className="form-group d-flex justify-content-between align-items-center">
-                  <label className="mb-0">{_('Email')}</label>
-                  <div className="custom-control custom-switch">
-                    <Field name="isEnableEmail" type="checkbox" className="custom-control-input" id="switch-notification-target-mail"/>
-                    <label className="custom-control-label" htmlFor="switch-notification-target-mail">
-                      <span>{_('ON')}</span>
-                      <span>{_('OFF')}</span>
-                    </label>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <div className="card">
-                    <div className="card-body">
-                      <div className={classNames('form-group', {'d-none': values.type === NotificationCardType.digitalInput})}>
-                        <label className="text-size-16 mb-3">{_('Email Attachment')}</label>
-                        <div className="select-wrapper border rounded-pill overflow-hidden">
-                          <Field
-                            name="emailAttachmentType"
-                            component="select"
-                            className="form-control border-0"
-                          >
-                            {
-                              NotificationEmailAttachmentType.all().map(attachmentType => (
-                                !(values.type === NotificationCardType.motionDetection && attachmentType === NotificationEmailAttachmentType.faceThumbnail) && (
-                                  <option
-                                    key={attachmentType}
-                                    value={attachmentType}
-                                  >{_(`email-attachment-type-${attachmentType}`)}
-                                  </option>)
-                              ))
-                            }
-                          </Field>
-                        </div>
-                        <hr/>
-                      </div>
-                      <div className="form-group">
-                        <label className="text-size-16 mb-3">{_('Receiver')}</label>
-                        <div className="form-row">
-                          <div className="col-auto my-1">
-                            <div className="input-group">
-                              <div className="input-group-prepend">
-                                <span
-                                  className="input-group-text"
-                                  style={{borderColor: (errors.$email && touched.$email) && '#dc3545'}}
-                                >
-                                  <i className="fas fa-envelope"/>
-                                </span>
-                              </div>
-                              <Field
-                                name="$email"
-                                type="text"
-                                className={classNames('form-control', 'notification-email', {'is-invalid': errors.$email && touched.$email})}
-                                validate={validateEmail}
-                                placeholder={_('Enter email address')}/>
-                            </div>
-                          </div>
-                          <CustomTooltip show={!values.$email} title={_('Please Enter an Email Address')}>
-
-                            <div className="col-auto my-1">
-                              <button
-                                disabled={!values.$email}
-                                style={values.$email ? {} : {pointerEvents: 'none'}}
-
-                                type="button"
-                                className="btn btn-primary rounded-circle"
-                                onClick={onClickAddEmail}
-                              >
-                                <i className="fas fa-plus"/>
-                              </button>
-                            </div>
-                          </CustomTooltip>
-
-                        </div>
-                        <div className={classNames({'is-invalid': errors.$email && touched.$email})}>
-                          {
-                            errors.$email && touched.$email && (
-                              <div
-                                style={{display: (errors.$email && touched.$email) && 'block'}}
-                                className="invalid-feedback form-row"
-                              >
-                                {errors.$email}
-                              </div>
-                            )
-                          }
-                        </div>
-                      </div>
-                      {
-                        values.emails.map((email, index) => {
-                          const key = `${index}${email}`;
-                          return (
-                            <div key={key} className="border border-primary rounded-pill text-primary d-flex justify-content-between align-items-center filter-item mb-3">
-                              <div>{email}</div>
-                              <a href="#" onClick={generateDeleteEmailHandler(index)}>
-                                <i className="fas fa-times-circle fa-lg"/>
-                              </a>
-                            </div>
-                          );
-                        })
-                      }
-                    </div>
-                  </div>
-                </div>
+                <CardsFormSubject
+                  values={values}
+                  setFieldValue={setFieldValue}
+                  validateField={validateField}
+                  errors={errors}
+                  touched={touched}
+                />
               </Tab.Pane>
             </Tab.Content>
           </div>
