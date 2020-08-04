95313dc044f3150b64d4e0c37789b4255409d3db
Rodger <rodger.lo@androvideo.com>
2020-08-04 17:13:36 +0800

    add new SD volume progress bar component

---
+++ src/core/components/sd-volume-progress-bar.js
@@ -0,0 +1,57 @@
+const filesize = require('filesize');
+const PropTypes = require('prop-types');
+const React = require('react');
+const _ = require('../../languages');
+const CustomTooltip = require('./tooltip');
+
+module.exports = class SdVolumeProgressBar extends React.PureComponent {
+  static get propTypes() {
+    return {
+      sdTotal: PropTypes.number.isRequired,
+      sdUsage: PropTypes.number.isRequired,
+      percentageToHideText: PropTypes.number
+    };
+  }
+
+  static get defaultProps() {
+    return {percentageToHideText: 8};
+  }
+
+  render() {
+    const {sdUsage, sdTotal, percentageToHideText} = this.props;
+    const usedDiskPercentage = Math.ceil((sdUsage / sdTotal) * 100);
+    const freeDiskPercentage = 100 - usedDiskPercentage;
+    const freeDiskVolume = sdTotal - sdUsage;
+    return (
+      <div className="progress">
+        {
+          isNaN(usedDiskPercentage) ?
+            <div className="progress-bar"/> : (
+              <>
+                <CustomTooltip title={_('Used: {0}', [filesize(sdUsage)])}>
+                  <div className="progress-bar" style={{width: `${usedDiskPercentage}%`}}>
+                    {usedDiskPercentage > percentageToHideText ? `${usedDiskPercentage}%` : ''}
+                  </div>
+                </CustomTooltip>
+                {usedDiskPercentage && (
+                  <CustomTooltip title={_('Free: {0}', [filesize(freeDiskVolume)])}>
+
+                    <div
+                      className="progress-bar"
+                      style={{
+                        width: `${freeDiskPercentage}%`,
+                        backgroundColor: '#e9ecef',
+                        color: 'var(--gray-dark)'
+                      }}
+                    >
+                      {freeDiskPercentage > 8 ? `${freeDiskPercentage}%` : ''}
+                    </div>
+                  </CustomTooltip>
+                )}
+              </>
+            )
+        }
+      </div>
+    );
+  }
+};
