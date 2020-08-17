e1ed4ee85c56c3db9270cf0d2969952e16f8d521
BensonLiao <Benson.Liao@androvideo.com>
2020-08-17 11:06:47 +0800

    Refactor language resource setup flow and let webpack inject all the bundle assets

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,3 +1,11 @@
+window.languageResource = {};
+
+require('./en-us');
+require('./es-es');
+require('./ja-jp');
+require('./zh-cn');
+require('./zh-tw');
+
 const format = require('string-template');
 
 const languageResource = window.languageResource || {};
@@ -9,7 +17,7 @@ module.exports = (key, values) => {
   @param values {Array<String>}
   @returns {String}
    */
-  const template = languageResource[key] || key;
+  const template = languageResource[window.currentLanguageCode || 'en-us'][key] || key;
 
   return values ? format(template, values) : template;
 };
