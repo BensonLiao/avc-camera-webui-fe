f5a4c8c37903a50bf54eaeeaaf22fda39f2faaf2
rodger.lo <rodger.lo@androvideo.com>
2020-08-31 18:03:40 +0800

    open image in new window

--- src/web/pages/events/events-table.js
+++ src/web/pages/events/events-table.js
@@ -67,10 +67,20 @@ module.exports = class EventsTable extends React.PureComponent {
   }
 
   generateEnlargePhotoHandler = eventPhoto => {
-    this.setState({
-      enlargePhoto: eventPhoto,
-      isShowEnlargeModal: true
-    });
+    const imageUrl = window.URL.createObjectURL(this.b64toBlob(eventPhoto));
+    window.open(imageUrl, '_blank', 'rel=noopener noreferrer');
+  }
+
+  b64toBlob = dataURI => {
+    var byteString = atob(dataURI.split(',')[1]);
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ab], {type: 'image/jpeg'});
   }
 
   render() {
