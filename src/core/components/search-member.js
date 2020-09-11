9a20fa7538c661b3fe07a7853910b9759b485a64
rodger.lo <rodger.lo@androvideo.com>
2020-09-11 11:24:44 +0800

    add pagination component

--- src/core/components/search-member.js
+++ src/core/components/search-member.js
@@ -168,7 +168,7 @@ class SearchMember extends React.PureComponent {
 
   render() {
     const {memberName, eventPictureUrl, isApiProcessing, isShowModal, onHide} = this.props;
-    const {members, maxIndex, isFetching, isVerifying, verifyStatus, errorMessage, convertedPicture} = this.state;
+    const {members, isFetching, isVerifying, verifyStatus, errorMessage, convertedPicture} = this.state;
     console.log(this.state);
     return (
       <>
@@ -354,16 +354,15 @@ class SearchMember extends React.PureComponent {
                       </tr>
                     )
                   }
-                  {
-                  // End of search
-                    members && members.index === maxIndex && members.items.length !== 0 && (
-                      <tr>
-                        <td className="text-size-16 text-center" colSpan="10">
-                          {_('End of result')}
-                        </td>
-                      </tr>
-                    )
-                  }
+                  { members && (
+                    <Pagination
+                      index={members.index}
+                      size={members.size}
+                      total={members.total}
+                      itemQuantity={members.items.length}
+                      onSearch={this.onSearch}
+                    />
+                  )}
                 </tbody>
               </table>
             </div>
@@ -384,3 +383,138 @@ class SearchMember extends React.PureComponent {
 }
 
 export default SearchMember;
+
+class Pagination extends React.PureComponent {
+  static propTypes = {
+    index: PropTypes.number.isRequired,
+    size: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    itemQuantity: PropTypes.number.isRequired,
+    onSearch: PropTypes.func.isRequired
+  }
+
+  constructor(props) {
+    super(props);
+    this.maxGotoIndex = Math.ceil(this.props.total / this.props.size);
+  }
+
+   onChangeGotoIndex = event => {
+     let validateValue = event.currentTarget.value;
+     if (Number(event.currentTarget.value)) {
+       validateValue = event.currentTarget.value >= this.maxGotoIndex ?
+         this.maxGotoIndex :
+         event.currentTarget.value;
+       validateValue = validateValue < 1 ? 1 : validateValue;
+       this.setState({gotoIndex: validateValue - 1});
+     }
+   }
+
+   onKeyPress = event => {
+     if (event.charCode === 13) {
+       this.props.onSearch(this.state.gotoIndex);
+     }
+   }
+
+   render() {
+     const {
+       index,
+       size,
+       total,
+       itemQuantity,
+       onSearch
+     } = this.props;
+     if (total === 0) {
+       return <></>;
+     }
+
+     const numbers = [];
+     const hasPrevious = index > 0;
+     const hasNext = total > (index + 1) * size;
+     const startItem = (index * size) + 1;
+     const endItem = startItem + itemQuantity - 1;
+     const {gotoIndex} = this.state;
+     for (let idx = index - 3; idx < index + 3; idx += 1) {
+       if (idx < 0 || idx >= this.maxGotoIndex) {
+         continue;
+       }
+
+       numbers.push({
+         key: `pagination-${idx}`,
+         pageNumber: idx + 1,
+         onClick: this.props.onSearch(idx),
+         className: classNames('page-item', {disabled: idx === index})
+       });
+     }
+
+     return (
+       <div className="col-12">
+         <nav
+           className="d-flex justify-content-center align-items-center"
+           style={{
+             padding: '0px 2px',
+             height: '36px'
+           }}
+         >
+           <p className="text-size-14 text-muted mb-0 mr-auto invisible">
+             {_('{0}-{1} items. Total: {2}', [startItem, endItem, total])}
+           </p>
+           <ul className="pagination my-auto">
+             <li className={classNames('page-item', {disabled: !hasPrevious})}>
+               <a
+                 className="page-link prev"
+                 onClick={() => {
+                   onSearch(index - 1);
+                 }}
+               >
+         &laquo;
+               </a>
+             </li>
+             {
+               numbers.map(number => (
+                 <li key={number.key} className={number.className}>
+                   <a className="page-link" onClick={number.onClick}>
+                     {number.pageNumber}
+                   </a>
+                 </li>
+               ))
+             }
+             <li className={classNames('page-item', {disabled: !hasNext})}>
+               <a
+                 className="page-link next"
+                 onClick={hasNext ? () => {
+                   onSearch(index + 1);
+                 } : ''}
+               >
+         &raquo;
+               </a>
+             </li>
+             <li className="page-item">
+               <input
+                 type="number"
+                 className="page-input"
+                 min={1}
+                 max={this.maxGotoIndex}
+                 onChange={this.onChangeGotoIndex}
+                 onKeyPress={this.onKeyPress}
+               />
+             </li>
+             <li className="page-item">
+               <a
+                 className="page-link go"
+                 onClick={() => {
+                   onSearch(gotoIndex);
+                 }}
+               >
+         Go
+               </a>
+             </li>
+           </ul>
+           <p className="text-size-14 text-muted mb-0 ml-auto">
+             {_('{0}-{1} items. Total: {2}', [startItem, endItem, total])}
+           </p>
+         </nav>
+       </div>
+     );
+   }
+}
+
