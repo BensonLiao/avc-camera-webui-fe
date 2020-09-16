7af779f36c91f3c4e6cce8921ff8940a40916136
rodger.lo <rodger.lo@androvideo.com>
2020-09-16 16:28:49 +0800

    get FR auth key type

--- src/web/router.js
+++ src/web/router.js
@@ -263,7 +263,8 @@ module.exports = new Router({
       resolve: {
         groups: () => api.group.getGroups().then(response => response.data),
         members: params => api.member.getMembers(params).then(response => response.data),
-        totalPictureCount: () => api.member.totalCount().then(response => response.data)
+        totalPictureCount: () => api.member.totalCount().then(response => response.data),
+        authKeys: () => api.authKey.getAuthKeys().then(response => response.data)
       },
       loadComponent: () => import(
         /* webpackChunkName: "page-members" */
