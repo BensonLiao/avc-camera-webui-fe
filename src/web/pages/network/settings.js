4b9a77d7d98e6b180ffe8b2eb0a60464013a0ff0
rodger.lo <rodger.lo@androvideo.com>
2020-09-18 15:38:06 +0800

    refactor proptypes and clean up code

--- src/web/pages/network/settings.js
+++ src/web/pages/network/settings.js
@@ -9,18 +9,7 @@ const {default: SettingsStatus} = require('./settings-status');
 
 module.exports = class NetworkSettings extends Base {
   static get propTypes() {
-    return {
-      networkSettings: PropTypes.shape({
-        networkInterface: PropTypes.string.isRequired,
-        ipType: PropTypes.string.isRequired,
-        ipAddress: PropTypes.string.isRequired,
-        primaryDNS: PropTypes.string.isRequired,
-        secondaryDNS: PropTypes.string.isRequired,
-        gateway: PropTypes.string.isRequired,
-        subnetMask: PropTypes.string.isRequired,
-        mac: PropTypes.string.isRequired
-      }).isRequired
-    };
+    return {networkSettings: PropTypes.shape(SettingsLan.propTypes.networkSettings).isRequired};
   }
 
   render() {
@@ -38,12 +27,10 @@ module.exports = class NetworkSettings extends Base {
                         {_('Internet/Network Settings')}
                       </Link>
                     </li>
-
                     <li className="breadcrumb-item">{_('Network Settings')}</li>
                   </ol>
                 </nav>
               </div>
-
               <div className="col-center">
                 <div className="card shadow">
                   <div className="card-header">{_('Network Settings')}</div>
@@ -67,23 +54,21 @@ module.exports = class NetworkSettings extends Base {
                     <div className="card-body">
                       <Tab.Content>
                         <Tab.Pane eventKey="tab-local-network">
-                          {/* <Formik
-                            initialValues={networkSettings}
-                            onSubmit={this.onSubmit}
-                          >
-                            {this.networkSettingsFormRender}
-                          </Formik> */}
+
                           <SettingsLan
                             networkSettings={networkSettings}
                             isApiProcessing={this.state.$isApiProcessing}
                           />
+
                         </Tab.Pane>
                       </Tab.Content>
                       <Tab.Content>
                         <Tab.Pane eventKey="tab-network-status">
+
                           <SettingsStatus
                             networkSettings={networkSettings}
                           />
+
                         </Tab.Pane>
                       </Tab.Content>
                     </div>
