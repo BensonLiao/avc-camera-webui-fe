a24b14ce3b28d1e7933c26bda0b8d67fc1dc2714
rodger.lo <rodger.lo@androvideo.com>
2020-09-21 11:46:44 +0800

    add breadcrumb component with path and routes

---
+++ src/core/components/fields/breadcrumb.js
@@ -0,0 +1,40 @@
+import classNames from 'classnames';
+import {Link} from 'capybara-router';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const BreadCrumb = props => {
+  const {path, routes, className} = props;
+  return (
+    <div className={classNames('col-12', className)}>
+      <nav>
+        <ol className="breadcrumb rounded-pill">
+          {path.map((value, index) => {
+            return (
+              path.length === index + 1 ?
+                (<li key={value} className="breadcrumb-item">{value}</li>) :
+                (
+                  <li key={value} className="breadcrumb-item active">
+                    <Link to={routes[index]}>{value}</Link>
+                  </li>
+                )
+            );
+          })}
+        </ol>
+      </nav>
+    </div>
+  );
+};
+
+BreadCrumb.propTypes = {
+  path: PropTypes.array.isRequired,
+  routes: PropTypes.array,
+  className: PropTypes.string
+};
+
+BreadCrumb.defaultProps = {
+  className: '',
+  routes: []
+};
+
+export default BreadCrumb;
