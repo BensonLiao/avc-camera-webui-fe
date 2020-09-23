813098d52171afe9e484f355e8f2495be6dadf55
kevin.kao <Kevin.Kao@androvideo.com>
2020-09-23 18:18:34 +0800

    remove all forgot-password related pages

--- src/web/validations/account/reset-password-validator.js
+++
@@ -1,4 +0,0 @@
-const {validator} = require('../../../core/validations');
-const UserSchema = require('webserver-form-schema/user-schema');
-
-module.exports = validator.compile({newPassword: UserSchema.password});
