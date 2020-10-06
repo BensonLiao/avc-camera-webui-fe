4ffe124ecce1980bc8f90514fe348da110fd0b57
rodger.lo <rodger.lo@androvideo.com>
2020-10-06 10:26:50 +0800

    update i18n config

--- src/web/i18n.js
+++ src/web/i18n.js
@@ -1,16 +1,39 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-require('../languages/en-us');
-require('../languages/zh-tw');
 
+window.languageResource = {};
 let languageCode = window.currentLanguageCode;
+
+switch (languageCode) {
+  case 'es-es':
+    require('./es-es');
+    break;
+  case 'ja-jp':
+    require('./ja-jp');
+    break;
+  case 'zh-cn':
+    require('./zh-cn');
+    break;
+  case 'zh-tw':
+    require('./zh-tw');
+    break;
+  default:
+    require('./en-us');
+    break;
+}
+
+// Capitalise country code, remove after updating backend AVC api string
 languageCode = languageCode.substring(0, 3) + languageCode.substring(3, 5).toUpperCase();
 
-// the translations
+// Translations
 const resources = {
   'en-US': {translation: window.languageResource['en-us']},
-  'zh-TW': {translation: window.languageResource['zh-tw']}
+  'zh-TW': {translation: window.languageResource['zh-tw']},
+  'zh-CN': {translation: window.languageResource['zh-cn']},
+  'ja-JP': {translation: window.languageResource['ja-jp']},
+  'es-ES': {translation: window.languageResource['es-es']}
 };
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
