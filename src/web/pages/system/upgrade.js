75f31963ac913759da1cca4c8a41a2f15533f541
rodger.lo <rodger.lo@androvideo.com>
2020-10-08 10:58:17 +0800

    /upgrade: remove old processing modal title text

--- src/web/pages/system/upgrade.js
+++ src/web/pages/system/upgrade.js
@@ -18,7 +18,7 @@ module.exports = class Upgrade extends Base {
     super(props);
     this.state.file = null;
     this.state.isShowApiProcessModal = false;
-    this.state.apiProcessModalTitle = _('Device Processing');
+    this.state.apiProcessModalTitle = _('Uploading Firmware');
     this.state.apiProcessModalBody = _('※ Please do not close browser or tab during upgrade');
     this.state.progressStatus = {
       uploadFirmware: 'initial',
@@ -152,7 +152,6 @@ module.exports = class Upgrade extends Base {
     progress.start();
     this.setState({
       isShowApiProcessModal: true,
-      apiProcessModalTitle: _('Uploading Firmware'),
       progressStatus: {
         uploadFirmware: 'start',
         upgradeFirmware: 'initial',
