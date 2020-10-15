49eed93205127ffac6445a8709b1ce6582580062
BensonLiao <Benson.Liao@androvideo.com>
2020-10-15 10:23:43 +0800

    feat: add loading indicator and gray out new address before clickable

--- src/web/pages/network/https.js
+++ src/web/pages/network/https.js
@@ -13,6 +13,7 @@ const CustomNotifyModal = require('../../../core/components/custom-notify-modal'
 const CustomTooltip = require('../../../core/components/tooltip');
 const SelectField = require('../../../core/components/fields/select-field');
 const BreadCrumb = require('../../../core/components/fields/breadcrumb').default;
+const infoColor = getComputedStyle(document.documentElement).getPropertyValue('--info');
 
 module.exports = class HTTPS extends Base {
   static get propTypes() {
@@ -80,7 +81,7 @@ module.exports = class HTTPS extends Base {
           modalBody: [
             `${i18n.t('Please Redirect Manually to the New Address')} :`,
             <div key="redirect" className="d-flex">
-              <div className="loading-spinners">
+              <div className="loading-spinners ml-0">
                 <div className="loading-dots">
                   <div className="spinner">
                     <div className="double-bounce1"/>
@@ -88,9 +89,26 @@ module.exports = class HTTPS extends Base {
                   </div>
                 </div>
               </div>
-              <a href={newAddress}>{newAddress}</a>
+              <span style={{color: infoColor}}>{newAddress}</span>
             </div>
           ]
+        }, () => {
+          setTimeout(() => {
+            this.setState({
+              modalBody: [
+                `${i18n.t('Please Redirect Manually to the New Address')} :`,
+                <div key="redirect" className="d-flex">
+                  <div className="loading-spinners ml-0">
+                    <svg className="checkmark show" viewBox="0 0 52 52">
+                      <circle className="checkmark-circle" cx="26" cy="26" r="25" fill="none"/>
+                      <path className="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+                    </svg>
+                  </div>
+                  <a href={newAddress}>{newAddress}</a>
+                </div>
+              ]
+            });
+          }, 3000);
         });
       })
       .finally(progress.done);
