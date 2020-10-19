b25cf379116404d381ee563c5ce73fd55715c37e
BensonLiao <Benson.Liao@androvideo.com>
2020-10-19 14:01:32 +0800

    refactor: make loading indicator as a reusable component

--- src/core/components/stage-progress.js
+++ src/core/components/stage-progress.js
@@ -1,6 +1,7 @@
 const PropTypes = require('prop-types');
 const React = require('react');
 const classNames = require('classnames');
+const LoadingIndicator = require('../components/loading-indicator').default;
 
 module.exports = class StageProgress extends React.PureComponent {
   static get propTypes() {
@@ -30,26 +31,11 @@ module.exports = class StageProgress extends React.PureComponent {
     return (
       <div className={classNames('stage-progress', {draw: progressStatus === 'done'})}>
         <div className="rounded-border-container">
-          <div className="loading-spinners">
-            <svg className={classNames('checkmark', {show: progressStatus === 'done'})} viewBox="0 0 52 52">
-              <circle className="checkmark-circle" cx="26" cy="26" r="25" fill="none"/>
-              <path className="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
-            </svg>
-            <svg className={classNames('crossmark', {show: progressStatus === 'fail'})} viewBox="0 0 52 52">
-              <circle className="crossmark-circle" cx="26" cy="26" r="25" fill="none"/>
-              <path className="crossmark-cross" fill="none" d="M16 16 36 36 M36 16 16 36"/>
-            </svg>
-            <div className={classNames('loading-dots', {'hide-loading': progressStatus !== 'start'})}>
-              {progressPercentage === -1 ? (
-                <div className="spinner">
-                  <div className="double-bounce1"/>
-                  <div className="double-bounce2"/>
-                </div>
-              ) : (
-                <div className={`pie-wrapper pie-wrapper--solid progress-${progressPercentage}`}/>
-              )}
-            </div>
-          </div>
+          <LoadingIndicator
+            isDetermined={progressPercentage !== -1}
+            percentage={progressPercentage}
+            status={progressStatus}
+          />
           <div className="stage">{stage}</div>
           <div className={classNames('divider', {draw: progressStatus === 'done'})}/>
           <div className="progress-title">{title}</div>
