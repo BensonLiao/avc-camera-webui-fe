97ec40667814a3fa3363db46d8ed167d7a0511ca
BensonLiao <Benson.Liao@androvideo.com>
2020-10-23 18:04:13 +0800

    refactor(i18n): load language resource with dynamic path

--- backend/handlers/base-handler.js
+++ backend/handlers/base-handler.js
@@ -4,7 +4,7 @@ exports.baseView = (req, res) => {
    */
   res.render('index', {
     htmlLang: 'en',
-    languageCode: 'zh-tw',
+    languageCode: 'en',
     loadingText: 'Loading',
     user: {
       id: 1,
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,25 +4,7 @@ const SUPPORTED_LANGUAGE_CODES = require('webserver-form-schema/constants/i18n-s
 const SUPPORTED_LANGUAGE_CODE_LIST = Object.keys(SUPPORTED_LANGUAGE_CODES);
 
 window.languageResource = {};
-let currentLanguageResource;
-// Assign resource to window.languageResource depends on current language code
-switch (window.currentLanguageCode) {
-  case 'es':
-    currentLanguageResource = require('../languages/es');
-    break;
-  case 'zh-tw':
-    currentLanguageResource = require('../languages/zh-tw');
-    break;
-  case 'zh-cn':
-    currentLanguageResource = require('../languages/zh-cn');
-    break;
-  case 'ja':
-    currentLanguageResource = require('../languages/ja');
-    break;
-  default:
-    currentLanguageResource = require('../languages/en');
-    break;
-}
+const currentLanguageResource = require(`../languages/${window.currentLanguageCode}`);
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
