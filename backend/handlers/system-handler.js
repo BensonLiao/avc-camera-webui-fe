add1b05ee93bb4aa0c7fe99b31e5dfbcc2a27887
rodger.lo <rodger.lo@androvideo.com>
2020-10-27 15:31:58 +0800

    allow setting language in express handler

--- backend/handlers/system-handler.js
+++ backend/handlers/system-handler.js
@@ -1,6 +1,9 @@
+const cache = require('memory-cache');
+
 exports.updateLanguage = (req, res) => {
   /*
   PUT /api/system/language
    */
+  cache.put('language', req.body.language);
   res.json(req.body);
 };
