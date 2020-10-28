6631f0d7153ea4dfb1331d7949039e4bc06ecdf5
rodger.lo <rodger.lo@androvideo.com>
2020-10-28 15:42:46 +0800

    license-status: rename props for bool values

--- src/web/pages/license/license-status.js
+++ src/web/pages/license/license-status.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import i18n from '../../../i18n';
 
-const LicenseStatus = ({licenseName, licenseKeyStatus, licenseEnableImg, licenseDisableImg, hide}) => {
+const LicenseStatus = ({licenseName, isEnabled, licenseEnableImg, licenseDisableImg, isHide}) => {
   return (
     <div className={classNames(
       'border text-center bg-white',
-      {'active shadow border-success': licenseKeyStatus},
-      {'d-none': hide})}
+      {'active shadow border-success': isEnabled},
+      {'d-none': isHide})}
     >
       <div className="img-wrapper">
-        <img src={licenseKeyStatus ? licenseEnableImg : licenseDisableImg}/>
+        <img src={isEnabled ? licenseEnableImg : licenseDisableImg}/>
       </div>
       <h4 className={classNames(
         'text-size-20 mt-3',
-        licenseKeyStatus ? 'text-primary' : 'text-muted')}
+        isEnabled ? 'text-primary' : 'text-muted')}
       >
         {licenseName}
       </h4>
@@ -23,13 +23,13 @@ const LicenseStatus = ({licenseName, licenseKeyStatus, licenseEnableImg, license
         <hr/>
         <span className={classNames(
           'border rounded-pill p-1 pr-2',
-          licenseKeyStatus ? 'border-success text-success' : 'border-danger text-danger')}
+          isEnabled ? 'border-success text-success' : 'border-danger text-danger')}
         >
           <i className={classNames(
             'fas mr-1',
-            licenseKeyStatus ? 'fa-check-circle' : 'fa-minus-circle')}
+            isEnabled ? 'fa-check-circle' : 'fa-minus-circle')}
           />
-          {licenseKeyStatus ? i18n.t('Activated') : i18n.t('Activation Required')}
+          {isEnabled ? i18n.t('Activated') : i18n.t('Activation Required')}
         </span>
       </div>
     </div>
@@ -38,13 +38,13 @@ const LicenseStatus = ({licenseName, licenseKeyStatus, licenseEnableImg, license
 
 LicenseStatus.propTypes = {
   licenseName: PropTypes.string.isRequired,
-  licenseKeyStatus: PropTypes.bool.isRequired,
+  isEnabled: PropTypes.bool.isRequired,
   licenseEnableImg: PropTypes.string.isRequired,
   licenseDisableImg: PropTypes.string.isRequired,
-  // Option to hide license component for specific camera models
-  hide: PropTypes.bool
+  // Option to isHide license component for specific camera models
+  isHide: PropTypes.bool
 };
 
-LicenseStatus.defaultProps = {hide: false};
+LicenseStatus.defaultProps = {isHide: false};
 
 export default LicenseStatus;
