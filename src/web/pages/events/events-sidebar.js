7e6c01d79e337942599791103f3b972a1679c3e6
rodger.lo <rodger.lo@androvideo.com>
2020-11-09 17:10:01 +0800

    /events: split sidebar by analytic type

--- src/web/pages/events/events-sidebar.js
+++ src/web/pages/events/events-sidebar.js
@@ -3,20 +3,12 @@ import {Link, getRouter} from 'capybara-router';
 import PropTypes from 'prop-types';
 import React from 'react';
 import i18n from '../../../i18n';
-import Similarity from 'webserver-form-schema/constants/event-filters/similarity';
-import RecognitionType from 'webserver-form-schema/constants/event-filters/recognition-type';
+import EventsSidebarFR from './events-sidebar-fr';
 
 const EventsSidebar = ({
   authStatus: {isEnableFaceRecognitionKey, isEnableAgeGenderKey, isEnableHumanoidDetectionKey},
   type, currentRouteName, params, isApiProcessing
 }) => {
-  /**
-   * Convert router params to an array.
-   * @param {Array<String>|String|null} param
-   * @returns {Array<String>}
-   */
-  const convertArrayParams = param => Array.isArray(param) ? [...param] : (param ? [param] : []);
-
   /**
    * Clear filter on user clicks.
    * @param {*} event
@@ -28,105 +20,6 @@ const EventsSidebar = ({
       name: currentRouteName,
       params: {type: type}
     });
-  };
-
-  /**
-   * Generate the handler to toggle filter.
-   * @param {String} paramKey
-   * @param {String} value
-   * @returns {Function} The handler.
-   */
-  const toggleFilterHandler = (paramKey, value) => () => {
-    const localParams = convertArrayParams(params[paramKey]);
-    const indexOfConfidences = localParams.indexOf(value);
-
-    if (indexOfConfidences >= 0) {
-      localParams.splice(indexOfConfidences, 1);
-    } else {
-      localParams.push(value);
-    }
-
-    getRouter().go({
-      name: currentRouteName,
-      params: {
-        ...params,
-        index: undefined,
-        [paramKey]: localParams
-      }
-    });
-  };
-
-  const faceRecognitionFilterRender = () => {
-    const confidence = convertArrayParams(params.confidence);
-    const enrollStatus = convertArrayParams(params.enrollStatus);
-    const similarityRender = [
-      {
-        confidence: Similarity.low,
-        id: 'input-checkbox-low-similar'
-      },
-      {
-        confidence: Similarity.medium,
-        id: 'input-checkbox-medium-similar'
-      },
-      {
-        confidence: Similarity.high,
-        id: 'input-checkbox-high-similar'
-      }
-    ];
-    const recognitionTypeRender = [
-      {
-        status: RecognitionType.registered,
-        id: 'input-checkbox-register'
-      },
-      {
-        status: RecognitionType.unknown,
-        id: 'input-checkbox-anonymous'
-      },
-      {
-        status: RecognitionType.fake,
-        id: 'input-checkbox-fake'
-      }
-    ];
-    return (
-      <div className="card-body">
-        <span>{i18n.t('Similarity')}</span>
-        <div className="checkbox-group mt-3 pl-2">
-          {similarityRender.map(item => (
-            <div key={item.id} className="form-check mb-3">
-              <input
-                type="checkbox"
-                className="form-check-input"
-                id={item.id}
-                disabled={isApiProcessing}
-                checked={confidence.indexOf(item.confidence) >= 0}
-                onChange={toggleFilterHandler('confidence', item.confidence)}
-              />
-              <label className="form-check-label" htmlFor={item.id}>
-                {i18n.t(`confidence-${item.confidence}`)}
-              </label>
-            </div>
-          ))}
-        </div>
-        <span>{i18n.t('Recognition Result')}</span>
-        <div className="checkbox-group mt-3 mb-2 pl-2">
-          {recognitionTypeRender.map((item, idx) => (
-            <div key={item.id} className={classNames('form-check', {'mb-3': idx < recognitionTypeRender.length - 1})}>
-              <input
-                type="checkbox"
-                className="form-check-input"
-                id={item.id}
-                disabled={isApiProcessing}
-                checked={enrollStatus.indexOf(item.status) >= 0}
-                onChange={toggleFilterHandler('enrollStatus', item.status)}
-              />
-              <label className="form-check-label" htmlFor={item.id}>
-                {i18n.t(`enroll-status-${item.status}`)}
-              </label>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
   };
 
   return (
@@ -157,7 +50,13 @@ const EventsSidebar = ({
               )
             }
           </div>
-          {type === 'face-recognition' && isEnableFaceRecognitionKey && faceRecognitionFilterRender()}
+          {type === 'face-recognition' && isEnableFaceRecognitionKey && (
+            <EventsSidebarFR
+              params={params}
+              isApiProcessing={isApiProcessing}
+              currentRouteName={currentRouteName}
+            />
+          )}
         </div>
         {/* Age & Gender Filter */}
         <div className={classNames('card sub mb-3 d-none', {active: type === 'age-gender' && isEnableAgeGenderKey})}>
@@ -215,24 +114,14 @@ const EventsSidebar = ({
 };
 
 EventsSidebar.propTypes = {
-  params: PropTypes.shape({
-    type: PropTypes.oneOf(['face-recognition', 'age-gender', 'humanoid-detection']),
-    confidence: PropTypes.oneOfType([
-      PropTypes.oneOf(Similarity.all()),
-      PropTypes.arrayOf(PropTypes.oneOf(Similarity.all()))
-    ]),
-    enrollStatus: PropTypes.oneOfType([
-      PropTypes.oneOf(RecognitionType.all()),
-      PropTypes.arrayOf(PropTypes.oneOf(RecognitionType.all()))
-    ])
-  }).isRequired,
+  params: PropTypes.shape(EventsSidebarFR.propTypes.params).isRequired,
   authStatus: PropTypes.shape({
     isEnableFaceRecognitionKey: PropTypes.bool.isRequired,
     isEnableAgeGenderKey: PropTypes.bool.isRequired,
     isEnableHumanoidDetectionKey: PropTypes.bool.isRequired
   }).isRequired,
   type: PropTypes.string.isRequired,
-  currentRouteName: PropTypes.string.isRequired,
+  currentRouteName: PropTypes.shape(EventsSidebarFR.propTypes.currentRouteName).isRequired,
   isApiProcessing: PropTypes.bool.isRequired
 };
 
