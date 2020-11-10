93fe3fc053676adac3f10e1b37d2f9fb89f6f740
kevin.kao <Kevin.Kao@androvideo.com>
2020-11-10 15:52:31 +0800

    refactor https.js to functional component

--- src/web/pages/network/https.js
+++ src/web/pages/network/https.js
@@ -1,53 +1,41 @@
-const classNames = require('classnames');
-const PropTypes = require('prop-types');
-const React = require('react');
-const progress = require('nprogress');
-const {Formik, Form, Field} = require('formik');
-const CertificateType = require('webserver-form-schema/constants/certificate-type');
-const Base = require('../shared/base');
-const i18n = require('../../../i18n').default;
-const utils = require('../../../core/utils');
-const api = require('../../../core/apis/web-api');
-const {DEFAULT_PORTS, NODE_SERVER_RESTART_DELAY_MS} = require('../../../core/constants');
-const CustomNotifyModal = require('../../../core/components/custom-notify-modal');
-const CustomTooltip = require('../../../core/components/tooltip');
-const SelectField = require('../../../core/components/fields/select-field');
-const BreadCrumb = require('../../../core/components/fields/breadcrumb').default;
-const ProgressIndicator = require('../../../core/components/progress-indicator').default;
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+import progress from 'nprogress';
+import {Formik, Form, Field} from 'formik';
+import CertificateType from 'webserver-form-schema/constants/certificate-type';
+import i18n from '../../../i18n';
+import utils from '../../../core/utils';
+import api from '../../../core/apis/web-api';
+import {DEFAULT_PORTS, NODE_SERVER_RESTART_DELAY_MS} from '../../../core/constants';
+import CustomNotifyModal from '../../../core/components/custom-notify-modal';
+import CustomTooltip from '../../../core/components/tooltip';
+import SelectField from '../../../core/components/fields/select-field';
+import BreadCrumb from '../../../core/components/fields/breadcrumb';
+import ProgressIndicator from '../../../core/components/progress-indicator';
+import withGlobalStatus from '../../withGlobalStatus';
+import {useContextState} from '../../stateProvider';
 
 const infoColor = getComputedStyle(document.documentElement).getPropertyValue('--info');
 
-module.exports = class HTTPS extends Base {
-  static get propTypes() {
-    return {
-      httpsSettings: PropTypes.shape({
-        isEnable: PropTypes.bool.isRequired,
-        port: PropTypes.string.isRequired,
-        certificateType: PropTypes.oneOf(CertificateType.all()).isRequired
-      }).isRequired,
-      httpInfo: PropTypes.shape({
-        port: PropTypes.string.isRequired,
-        port2: PropTypes.string
-      }).isRequired,
-      rtspSettings: PropTypes.shape({
-        tcpPort: PropTypes.string.isRequired,
-        udpPort: PropTypes.string.isRequired
-      }).isRequired
-    };
-  }
+const HTTPS = ({httpsSettings, rtspSettings, httpInfo}) => {
+  const {isApiProcessing} = useContextState();
 
-  constructor(props) {
-    super(props);
-    this.state.isShowModal = false;
-    this.state.modalBody = i18n.t('The website has been redirected to the new address');
-  }
+  const [state, setState] = useState({
+    isShowModal: false,
+    modalBody: i18n.t('The website has been redirected to the new address')
+  });
 
-  hideModal = () => {
-    this.setState({isShowModal: false});
+  const {isShowModal, modalBody} = state;
+
+  const hideModal = () => {
+    setState({
+      ...state,
+      isShowModal: false
+    });
   };
 
-  checkValidatePort = values => {
-    const {httpInfo, rtspSettings} = this.props;
+  const checkValidatePort = values => {
     let defaultPorts = DEFAULT_PORTS;
 
     let checkDefaultPortList = Object.keys(defaultPorts)
@@ -69,16 +57,17 @@ module.exports = class HTTPS extends Base {
     }
 
     return utils.validatedPortCheck(values);
-  }
+  };
 
-  onSubmitForm = values => {
+  const onSubmitForm = values => {
     progress.start();
     // Set delay time and wait for nodejs restart complete
     // Note. this is not a reliable solution cause the waiting time may vary from different environment
     api.system.updateHttpsSettings(values)
       .then(() => {
         const newAddress = `${values.isEnable ? 'https' : 'http'}://${location.hostname}${values.isEnable ? `:${values.port}` : ''}`;
-        this.setState({
+        setState({
+          ...state,
           isShowModal: true,
           modalBody: [
             `${i18n.t('The website has been redirected to the new address')} :`,
@@ -92,7 +81,8 @@ module.exports = class HTTPS extends Base {
           ]
         }, () => {
           setTimeout(() => {
-            this.setState({
+            setState({
+              ...state,
               modalBody: [
                 `${i18n.t('The website has been redirected to the new address')} :`,
                 <div key="redirect" className="d-flex">
@@ -110,99 +100,106 @@ module.exports = class HTTPS extends Base {
       .finally(progress.done);
   };
 
-  formRender = ({values, errors, touched}) => {
-    const {$isApiProcessing, isShowModal, modalBody} = this.state;
-    const {httpsSettings} = this.props;
-
-    return (
-      <Form className="card-body">
-        <div className="form-group d-flex justify-content-between align-items-center">
-          <label>{i18n.t('Enable HTTPS')}</label>
-          <div className="custom-control custom-switch">
-            <Field name="isEnable" checked={values.isEnable} type="checkbox" className="custom-control-input" id="switch-enable"/>
-            <label className="custom-control-label" htmlFor="switch-enable">
-              <span>{i18n.t('ON')}</span>
-              <span>{i18n.t('OFF')}</span>
-            </label>
-          </div>
-        </div>
-        <div className="form-group">
-          <label>{i18n.t('Port')}</label>
-          <Field
-            name="port"
-            type="text"
-            validate={this.checkValidatePort}
-            className={classNames('form-control', {'is-invalid': errors.port && touched.port})}
-          />
-          {
-            errors.port && touched.port && (
-              <div className="invalid-feedback">{errors.port}</div>
-            )
-          }
-          <p className="text-size-14 text-muted mt-2">{i18n.t('Range: 1024-65535 Default: 8443')}</p>
-        </div>
-        <SelectField labelName={i18n.t('Certificate')} name="certificateType">
-          <option value={CertificateType.selfSigned}>{i18n.t(`certificate-type-${CertificateType.selfSigned}`)}</option>
-        </SelectField>
-        <CustomTooltip show={(httpsSettings.isEnable === values.isEnable) && httpsSettings.isEnable === false} title={i18n.t('Please enable HTTPS first.')}>
-          <div>
-            <button
-              disabled={
-                $isApiProcessing ||
-                !utils.isObjectEmpty(errors) ||
-                ((httpsSettings.isEnable === values.isEnable) && httpsSettings.isEnable === false)
-              }
-              className="btn btn-primary btn-block rounded-pill"
-              type="submit"
-              style={(httpsSettings.isEnable === values.isEnable) && httpsSettings.isEnable === false ? {pointerEvents: 'none'} : {}}
-            >
-              {i18n.t('Apply')}
-            </button>
-          </div>
-        </CustomTooltip>
-        <CustomNotifyModal
-          isShowAllBtns={false}
-          backdrop="static"
-          isShowModal={isShowModal}
-          modalTitle={i18n.t('Redirection Success')}
-          modalBody={modalBody}
-          onConfirm={this.hideModal}
-          onHide={this.hideModal}
-        />
-      </Form>
-    );
-  };
-
-  render() {
-    const {httpsSettings} = this.props;
-    const {validator} = this.state;
-
-    return (
-      <div className="main-content left-menu-active">
-        <div className="page-notification">
-          <div className="container-fluid">
-            <div className="row">
-              <BreadCrumb
-                className="px-0"
-                path={[i18n.t('Internet & Network Settings'), i18n.t('HTTPS')]}
-                routes={['/network/settings']}
-              />
-              <div className="col-center">
-                <div className="card shadow">
-                  <div className="card-header">{i18n.t('HTTPS')}</div>
-                  <Formik
-                    initialValues={httpsSettings}
-                    validate={validator}
-                    onSubmit={this.onSubmitForm}
-                  >
-                    {this.formRender}
-                  </Formik>
-                </div>
+  return (
+    <div className="main-content left-menu-active">
+      <div className="page-notification">
+        <div className="container-fluid">
+          <div className="row">
+            <BreadCrumb
+              className="px-0"
+              path={[i18n.t('Internet & Network Settings'), i18n.t('HTTPS')]}
+              routes={['/network/settings']}
+            />
+            <div className="col-center">
+              <div className="card shadow">
+                <div className="card-header">{i18n.t('HTTPS')}</div>
+                <Formik
+                  initialValues={httpsSettings}
+                  onSubmit={onSubmitForm}
+                >
+                  {({values, errors, touched}) => {
+                    return (
+                      <Form className="card-body">
+                        <div className="form-group d-flex justify-content-between align-items-center">
+                          <label>{i18n.t('Enable HTTPS')}</label>
+                          <div className="custom-control custom-switch">
+                            <Field name="isEnable" checked={values.isEnable} type="checkbox" className="custom-control-input" id="switch-enable"/>
+                            <label className="custom-control-label" htmlFor="switch-enable">
+                              <span>{i18n.t('ON')}</span>
+                              <span>{i18n.t('OFF')}</span>
+                            </label>
+                          </div>
+                        </div>
+                        <div className="form-group">
+                          <label>{i18n.t('Port')}</label>
+                          <Field
+                            name="port"
+                            type="text"
+                            validate={checkValidatePort}
+                            className={classNames('form-control', {'is-invalid': errors.port && touched.port})}
+                          />
+                          {
+                            errors.port && touched.port && (
+                              <div className="invalid-feedback">{errors.port}</div>
+                            )
+                          }
+                          <p className="text-size-14 text-muted mt-2">{i18n.t('Range: 1024-65535 Default: 8443')}</p>
+                        </div>
+                        <SelectField labelName={i18n.t('Certificate')} name="certificateType">
+                          <option value={CertificateType.selfSigned}>{i18n.t(`certificate-type-${CertificateType.selfSigned}`)}</option>
+                        </SelectField>
+                        <CustomTooltip show={(httpsSettings.isEnable === values.isEnable) && httpsSettings.isEnable === false} title={i18n.t('Please enable HTTPS first.')}>
+                          <div>
+                            <button
+                              disabled={
+                                isApiProcessing ||
+                                !utils.isObjectEmpty(errors) ||
+                                ((httpsSettings.isEnable === values.isEnable) && httpsSettings.isEnable === false)
+                              }
+                              className="btn btn-primary btn-block rounded-pill"
+                              type="submit"
+                              style={(httpsSettings.isEnable === values.isEnable) && httpsSettings.isEnable === false ? {pointerEvents: 'none'} : {}}
+                            >
+                              {i18n.t('Apply')}
+                            </button>
+                          </div>
+                        </CustomTooltip>
+                        <CustomNotifyModal
+                          isShowAllBtns={false}
+                          backdrop="static"
+                          isShowModal={isShowModal}
+                          modalTitle={i18n.t('Redirection Success')}
+                          modalBody={modalBody}
+                          onConfirm={hideModal}
+                          onHide={hideModal}
+                        />
+                      </Form>
+                    );
+                  }}
+                </Formik>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
+
+HTTPS.propTypes = {
+  httpsSettings: PropTypes.shape({
+    isEnable: PropTypes.bool.isRequired,
+    port: PropTypes.string.isRequired,
+    certificateType: PropTypes.oneOf(CertificateType.all()).isRequired
+  }).isRequired,
+  httpInfo: PropTypes.shape({
+    port: PropTypes.string.isRequired,
+    port2: PropTypes.string
+  }).isRequired,
+  rtspSettings: PropTypes.shape({
+    tcpPort: PropTypes.string.isRequired,
+    udpPort: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default withGlobalStatus(HTTPS);
