310936ed0cd06e1b0684d268024e6c74e782f993
rodger.lo <rodger.lo@androvideo.com>
2020-11-20 10:12:03 +0800

    /accounts: move `add` button out of table component

--- src/web/pages/security/users-table.js
+++ src/web/pages/security/users-table.js
@@ -2,13 +2,12 @@ import classNames from 'classnames';
 import progress from 'nprogress';
 import PropTypes from 'prop-types';
 import React, {useState} from 'react';
-import {RouterView, Link, getRouter} from 'capybara-router';
+import {Link, getRouter} from 'capybara-router';
 import UserPermission from 'webserver-form-schema/constants/user-permission';
 import api from '../../../core/apis/web-api';
 import CustomNotifyModal from '../../../core/components/custom-notify-modal';
 import CustomTooltip from '../../../core/components/tooltip';
 import i18n from '../../../i18n';
-import {SECURITY_USERS_MAX} from '../../../core/constants';
 import {useContextState} from '../../stateProvider';
 
 const UsersTable = ({permissionFilter, users}) => {
@@ -31,8 +30,6 @@ const UsersTable = ({permissionFilter, users}) => {
       user.permission.toString() === UserPermission.root || user.permission.toString() === UserPermission.superAdmin :
       user.permission.toString() === permissionFilter || user.permission.toString() === UserPermission.viewer);
 
-  const isAddUserDisabled = usersList.length >= SECURITY_USERS_MAX;
-
   const confirmDeleteUser = event => {
     event.preventDefault();
     progress.start();
@@ -45,91 +42,70 @@ const UsersTable = ({permissionFilter, users}) => {
   };
 
   return (
-    <div className="main-content left-menu-active sub">
-      <div className="page-users bg-white">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-12 text-right mr-32px mb-4">
-              <Link
-                to={{name: 'web.users.accounts.new-user'}}
-                tabIndex={(isAddUserDisabled ? -1 : null)}
-                className={classNames(
-                  'btn btn-outline-primary rounded-pill px-3',
-                  {disabled: isAddUserDisabled}
-                )}
-              >
-                <i className="fas fa-plus fa-fw"/> {i18n.t('New')}
-              </Link>
-            </div>
-
-            <div className="col-12">
-              <table className="table custom-style">
-                <thead>
-                  <tr className="shadow">
-                    <th style={{width: '33%'}}>{i18n.t('Permission')}</th>
-                    <th style={{width: '34%'}}>{i18n.t('Username')}</th>
-                    <th style={{width: '33%'}}>{i18n.t('Actions')}</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    usersList.map((user, index) => {
-                      const tdClass = classNames({'border-bottom': index >= usersList.length - 1});
-                      const isSuperAdmin = user.permission === UserPermission.superAdmin;
-                      return (
-                        <tr key={user.id}>
-                          <td className={tdClass}>
-                            <span
-                              className={classNames(
-                                'badge badge-pill text-size-16 px-3',
-                                (user.permission === UserPermission.root || isSuperAdmin) ? 'badge-admin' : 'badge-guest'
-                              )}
-                            >
-                              {i18n.t(`permission-${user.permission}`)}
-                            </span>
-                          </td>
-                          <td className={tdClass}>{user.account}</td>
-                          <td className={classNames('text-left group-btn', tdClass)}>
-                            <Link
+    <>
+      <div className="col-12">
+        <table className="table custom-style">
+          <thead>
+            <tr className="shadow">
+              <th style={{width: '33%'}}>{i18n.t('Permission')}</th>
+              <th style={{width: '34%'}}>{i18n.t('Username')}</th>
+              <th style={{width: '33%'}}>{i18n.t('Actions')}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              usersList.map((user, index) => {
+                const tdClass = classNames({'border-bottom': index >= usersList.length - 1});
+                const isSuperAdmin = user.permission === UserPermission.superAdmin;
+                return (
+                  <tr key={user.id}>
+                    <td className={tdClass}>
+                      <span
+                        className={classNames(
+                          'badge badge-pill text-size-16 px-3',
+                          (user.permission === UserPermission.root || isSuperAdmin) ? 'badge-admin' : 'badge-guest'
+                        )}
+                      >
+                        {i18n.t(`permission-${user.permission}`)}
+                      </span>
+                    </td>
+                    <td className={tdClass}>{user.account}</td>
+                    <td className={classNames('text-left group-btn', tdClass)}>
+                      <Link
+                        className="btn btn-link"
+                        to={{
+                          name: 'web.users.accounts.details',
+                          params: {userId: user.id}
+                        }}
+                      >
+                        <i className="fas fa-pen fa-lg fa-fw"/>
+                      </Link>
+                      { !isSuperAdmin && (
+                        <CustomTooltip
+                          show={user.account === account}
+                          title={i18n.t('This account cannot be deleted because it is currently logged in to the device.')}
+                        >
+                          <span>
+                            <button
+                              // Super Admin account should not be deleted, due to app restrictions
+                              disabled={user.account === account}
                               className="btn btn-link"
-                              to={{
-                                name: 'web.users.accounts.details',
-                                params: {userId: user.id}
-                              }}
+                              type="button"
+                              onClick={generateShowDeleteUserModalHandler(user)}
                             >
-                              <i className="fas fa-pen fa-lg fa-fw"/>
-                            </Link>
-                            { !isSuperAdmin && (
-                              <CustomTooltip
-                                show={user.account === account}
-                                title={i18n.t('This account cannot be deleted because it is currently logged in to the device.')}
-                              >
-                                <span>
-                                  <button
-                                    // Super Admin account should not be deleted, due to app restrictions
-                                    disabled={user.account === account}
-                                    className="btn btn-link"
-                                    type="button"
-                                    onClick={generateShowDeleteUserModalHandler(user)}
-                                  >
-                                    <i className="far fa-trash-alt fa-lg fa-fw"/>
-                                  </button>
-                                </span>
-                              </CustomTooltip>
-                            )}
-                          </td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-        <RouterView/>
+                              <i className="far fa-trash-alt fa-lg fa-fw"/>
+                            </button>
+                          </span>
+                        </CustomTooltip>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
       </div>
-
       {/* Delete user modal */}
       <CustomNotifyModal
         isShowModal={isShowDeleteUserModal}
@@ -139,7 +115,7 @@ const UsersTable = ({permissionFilter, users}) => {
         onHide={() => setIsShowDeleteUserModal(false)}
         onConfirm={confirmDeleteUser}
       />
-    </div>
+    </>
   );
 };
 
--- src/web/pages/security/users.js
+++ src/web/pages/security/users.js
@@ -1,21 +1,48 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React, {useState} from 'react';
+import {RouterView, Link} from 'capybara-router';
+import i18n from '../../../i18n';
+import {SECURITY_USERS_MAX} from '../../../core/constants';
+import withGlobalStatus from '../../withGlobalStatus';
 import UsersSidebar from './users-sidebar';
 import UsersTable from './users-table';
-import withGlobalStatus from '../../withGlobalStatus';
 
 const Users = ({users}) => {
   const [permissionFilter, setPermissionFilter] = useState('all');
+  const isAddUserDisabled = usersList.length >= SECURITY_USERS_MAX;
   return (
     <>
       <UsersSidebar
         setPermissionFilter={setPermissionFilter}
         permissionFilter={permissionFilter}
       />
-      <UsersTable
-        users={users}
-        permissionFilter={permissionFilter}
-      />
+      <div className="main-content left-menu-active sub">
+        <div className="page-users bg-white">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-12 text-right mr-32px mb-4">
+                <Link
+                  to={{name: 'web.users.accounts.new-user'}}
+                  tabIndex={(isAddUserDisabled ? -1 : null)}
+                  className={classNames(
+                    'btn btn-outline-primary rounded-pill px-3',
+                    {disabled: isAddUserDisabled}
+                  )}
+                >
+                  <i className="fas fa-plus fa-fw"/> {i18n.t('New')}
+                </Link>
+              </div>
+              <UsersTable
+                users={users}
+                permissionFilter={permissionFilter}
+              />
+            </div>
+          </div>
+          <RouterView/>
+        </div>
+      </div>
+
     </>
   );
 };
