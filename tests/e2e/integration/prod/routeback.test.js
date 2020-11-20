85482a5e5c6dd40a9411dbd753a1eb8dd3c17c03
kevin.kao <Kevin.Kao@androvideo.com>
2020-11-20 18:46:18 +0800

    tests: add routeback test for issue

--- tests/e2e/integration/prod/routeback.test.js
+++ tests/e2e/integration/prod/routeback.test.js
@@ -8,18 +8,45 @@ describe('notification card page route back test', () => {
   });
 
   const repeatTimes = 100;
-  it(`go to notification card page then go back to previous url, test ${repeatTimes} times`, () => {
-    cy.visit('/notification/smtp');
-    for (let i = 0; i < repeatTimes; i++) {
-      cy.wait(3000)
-        .get('a[href="/notification/cards"]')
-        .click()
-        .get('div.filter')
-        .then(el => {
-          if (el) {
-            cy.go('back');
-          }
+
+  const routes = [
+    {
+      route: '/notification/smtp',
+      action: () => {
+        cy.wait(2000);
+      },
+      validation: index => {
+        cy.location().should(loc => {
+          expect(loc.pathname).to.eq(routes[index].route);
         });
+      }
+    },
+    {
+      route: '/app/com.avc.app.wiegandservice/',
+      action: () => {
+        cy.get('input[name="username"]').type(account);
+        cy.get('input[name="password"]').type(password);
+        cy.get('button[onclick="login()"]').click();
+        cy.wait(2000);
+        cy.go('back');
+      },
+      validation: index => {
+        cy.location().should(loc => {
+          expect(loc.pathname).to.eq(routes[index].route);
+        });
+      }
+    }
+  ];
+
+  it(`go to each route and validate navigated, run action per route, go back to first route, repeat ${repeatTimes} times`, () => {
+    for (let i = 0; i < repeatTimes; i++) {
+      routes.forEach((route, index) => {
+        cy.visit(route.route);
+        route.validation(index);
+        if (route.action) {
+          route.action();
+        }
+      });
     }
   });
 });
