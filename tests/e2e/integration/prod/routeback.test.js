318b893d524c1bc690d78db8252715ddc49b4de5
BensonLiao <Benson.Liao@androvideo.com>
2020-11-23 10:56:02 +0800

    chore(test): restore test case

--- tests/e2e/integration/prod/routeback.test.js
+++ tests/e2e/integration/prod/routeback.test.js
@@ -7,7 +7,7 @@ describe('notification card page route back test', () => {
     cy.login(account, password);
   });
 
-  const repeatTimes = 10;
+  const repeatTimes = 100;
   it(`go to notification card page then go back to previous url, test ${repeatTimes} times`, () => {
     cy.visit('/notification/smtp');
     for (let i = 0; i < repeatTimes; i++) {
