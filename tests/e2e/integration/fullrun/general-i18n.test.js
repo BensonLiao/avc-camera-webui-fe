560d14f744c7c86dca7ca8460274b7c8df5ef52d
kevin.kao <Kevin.Kao@androvideo.com>
2020-12-07 15:27:39 +0800

    upgrade cypress to latest version

--- tests/e2e/integration/fullrun/general-i18n.test.js
+++ tests/e2e/integration/fullrun/general-i18n.test.js
@@ -7,8 +7,11 @@ describe('license page test', () => {
     cy.login(account, password);
   });
 
-  it('should arrive at homepage', () => {
+  it('should arrive at homepage, wait for apis to load before navigating again', () => {
     cy.visit('/');
+    cy.intercept('GET', '/api/system/information').as('getInformation');
+    cy.intercept('GET', '/api/system/network').as('getNetworkSettings');
+    cy.intercept('GET', '/api/video/settings').as('getSettings');
   });
 });
 
