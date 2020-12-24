751dc4174ef3ccdfee25cc9cbda55997d768f01a
rodger.lo <rodger.lo@androvideo.com>
2020-12-24 18:45:29 +0800

    sync: add api process modal for adding/editing device

--- src/web/pages/members/members-deviceSync-add.js
+++ src/web/pages/members/members-deviceSync-add.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import {Formik, Form, Field, ErrorMessage} from 'formik';
@@ -9,9 +9,13 @@ import Password from '../../../core/components/fields/password';
 import {useContextState} from '../../stateProvider';
 import {getRouter} from '@benson.liao/capybara-router';
 import deviceSyncValidator from '../../validations/members/device-sync-validator';
+import CustomNotifyModal from '../../../core/components/custom-notify-modal';
 
 const DeviceSyncAddDevice = ({device, isShowModal, hideModal}) => {
   const {isApiProcessing} = useContextState();
+  const [isShowApiProcessModal, setIsShowApiProcessModal] = useState(false);
+
+  const hideApiProcessModal = () => setIsShowApiProcessModal(false);
 
   /**
    * Submit new or edit device info
@@ -37,95 +41,107 @@ const DeviceSyncAddDevice = ({device, isShowModal, hideModal}) => {
   };
 
   return (
-    <Modal autoFocus={false} show={isShowModal} backdrop={isApiProcessing ? 'static' : true} onHide={hideModal}>
-      <Modal.Header className="d-flex justify-content-between align-items-center">
-        <Modal.Title as="h5">
-          {device ?
-            i18n.t('demo.userManagement.members.modal.deviceSync.modifyDeviceTitle') :
-            i18n.t('demo.userManagement.members.modal.deviceSync.newDeviceTitle')}
-        </Modal.Title>
-      </Modal.Header>
-      <Formik
-        initialValues={device || {
-          ip: '',
-          port: '',
-          account: '',
-          password: ''
-        }}
-        validate={deviceSyncValidator}
-        onSubmit={onSubmitForm}
-      >
-        {({errors, touched}) => (
-          <Form>
-            <div className="modal-body">
-              <div className="row">
-                <div className="form-group col-8">
-                  <label>{i18n.t('demo.userManagement.members.host')}</label>
-                  <Field
-                    name="ip"
-                    type="text"
-                    placeholder={i18n.t('demo.userManagement.members.modal.deviceSync.hostPlaceholder')}
-                    className={classNames('form-control', {'is-invalid': errors.ip && touched.ip})}
-                  />
-                  <ErrorMessage component="div" name="ip" className="invalid-feedback"/>
+    <>
+      <Modal autoFocus={false} show={isShowModal} backdrop={isApiProcessing ? 'static' : true} onHide={hideModal}>
+        <Modal.Header className="d-flex justify-content-between align-items-center">
+          <Modal.Title as="h5">
+            {device ?
+              i18n.t('demo.userManagement.members.modal.deviceSync.modifyDeviceTitle') :
+              i18n.t('demo.userManagement.members.modal.deviceSync.newDeviceTitle')}
+          </Modal.Title>
+        </Modal.Header>
+        <Formik
+          initialValues={device || {
+            ip: '',
+            port: '',
+            account: '',
+            password: ''
+          }}
+          validate={deviceSyncValidator}
+          onSubmit={onSubmitForm}
+        >
+          {({errors, touched}) => (
+            <Form>
+              <div className="modal-body">
+                <div className="row">
+                  <div className="form-group col-8">
+                    <label>{i18n.t('demo.userManagement.members.host')}</label>
+                    <Field
+                      name="ip"
+                      type="text"
+                      placeholder={i18n.t('demo.userManagement.members.modal.deviceSync.hostPlaceholder')}
+                      className={classNames('form-control', {'is-invalid': errors.ip && touched.ip})}
+                    />
+                    <ErrorMessage component="div" name="ip" className="invalid-feedback"/>
+                  </div>
+                  <div className="form-group col-4">
+                    <label>{i18n.t('demo.userManagement.members.modal.deviceSync.port')}</label>
+                    <Field
+                      name="port"
+                      type="text"
+                      placeholder={i18n.t('demo.userManagement.members.modal.deviceSync.portPlaceholder')}
+                      className={classNames('form-control', {'is-invalid': errors.port && touched.port})}
+                    />
+                    <ErrorMessage component="div" name="port" className="invalid-feedback"/>
+                  </div>
                 </div>
-                <div className="form-group col-4">
-                  <label>{i18n.t('demo.userManagement.members.modal.deviceSync.port')}</label>
+                <div className="form-group">
+                  <label>{i18n.t('userManagement.accounts.username')}</label>
                   <Field
-                    name="port"
+                    name="account"
                     type="text"
-                    placeholder={i18n.t('demo.userManagement.members.modal.deviceSync.portPlaceholder')}
-                    className={classNames('form-control', {'is-invalid': errors.port && touched.port})}
+                    placeholder={i18n.t('userManagement.accounts.modal.usernamePlaceholder')}
+                    className={classNames('form-control', {'is-invalid': errors.account && touched.account})}
                   />
-                  <ErrorMessage component="div" name="port" className="invalid-feedback"/>
+                  <ErrorMessage component="div" name="account" className="invalid-feedback"/>
+                </div>
+                <div className="form-group has-feedback">
+                  <label>{i18n.t('common.password.password')}</label>
+                  <Field
+                    name="password"
+                    component={Password}
+                    inputProps={{
+                      placeholder: i18n.t('common.password.passwordPlaceholder'),
+                      className: classNames('form-control', {'is-invalid': errors.password && touched.password})
+                    }}
+                  />
+                  <ErrorMessage component="div" name="password" className="invalid-feedback"/>
                 </div>
               </div>
-              <div className="form-group">
-                <label>{i18n.t('userManagement.accounts.username')}</label>
-                <Field
-                  name="account"
-                  type="text"
-                  placeholder={i18n.t('userManagement.accounts.modal.usernamePlaceholder')}
-                  className={classNames('form-control', {'is-invalid': errors.account && touched.account})}
-                />
-                <ErrorMessage component="div" name="account" className="invalid-feedback"/>
-              </div>
-              <div className="form-group has-feedback">
-                <label>{i18n.t('common.password.password')}</label>
-                <Field
-                  name="password"
-                  component={Password}
-                  inputProps={{
-                    placeholder: i18n.t('common.password.passwordPlaceholder'),
-                    className: classNames('form-control', {'is-invalid': errors.password && touched.password})
-                  }}
-                />
-                <ErrorMessage component="div" name="password" className="invalid-feedback"/>
-              </div>
-            </div>
-            <div className="modal-footer flex-column">
-              <div className="form-group w-100 mx-0">
+              <div className="modal-footer flex-column">
+                <div className="form-group w-100 mx-0">
+                  <button
+                    disabled={isApiProcessing}
+                    type="submit"
+                    className="btn btn-primary btn-block rounded-pill"
+                  >
+                    {device ? i18n.t('common.button.confirm') : i18n.t('common.button.new')}
+                  </button>
+                </div>
                 <button
                   disabled={isApiProcessing}
-                  type="submit"
-                  className="btn btn-primary btn-block rounded-pill"
+                  className="btn btn-info btn-block m-0 rounded-pill"
+                  type="button"
+                  onClick={hideModal}
                 >
-                  {device ? i18n.t('common.button.confirm') : i18n.t('common.button.new')}
+                  {i18n.t('common.button.close')}
                 </button>
               </div>
-              <button
-                disabled={isApiProcessing}
-                className="btn btn-info btn-block m-0 rounded-pill"
-                type="button"
-                onClick={hideModal}
-              >
-                {i18n.t('common.button.close')}
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </Modal>
+            </Form>
+          )}
+        </Formik>
+      </Modal>
+      {/* Database updating modal */}
+      <CustomNotifyModal
+        modalType="process"
+        backdrop="static"
+        isShowModal={isShowApiProcessModal}
+        modalTitle={i18n.t(device ?
+          'demo.userManagement.members.modal.deviceSync.addDeviceApiProcessingModal' :
+          'demo.userManagement.members.modal.deviceSync.editDeviceApiProcessingModal')}
+        onHide={hideApiProcessModal}
+      />
+    </>
   );
 };
 
