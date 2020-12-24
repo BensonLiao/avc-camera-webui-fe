16daaf19ec80fc72af8f87c6e0b9833b2d940f23
benson.liao <benson.liao@androvideo.com>
2020-12-24 16:35:52 +0800

    refactor(error-page): simpify i18n caller syntax

--- src/web/pages/shared/error-page.js
+++ src/web/pages/shared/error-page.js
@@ -9,7 +9,7 @@ import i18n from '../../../i18n';
 const ErrorPage = ({error: {status}}) => {
   document.title = `${i18n.t('error.error')} - ${window.cameraName} Web-Manager`;
   // We need the following line because backend error data are different from i18n key for now
-  const messageTitle = i18n.t(`error.${status === 404 ? 'notFound' : 'internalServerError'}`);
+  const messageTitle = i18n.t(status === 404 ? 'error.notFound' : 'error.internalServerError');
   return (
     <div className="error-page">
       <img className="mw-100" src={status === 404 ? bgCode404 : bgCode500}/>
