542ed21f60314a0846ca0bced18de0d8fb27c322
benson.liao <benson.liao@androvideo.com>
2020-12-25 16:37:26 +0800

    feat(i18n): update config to output in current language resource folder and do not sorting keys

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -38,14 +38,14 @@ module.exports = {
   // If you want to use plain english keys, separators such as `.` and `:` will conflict.
   // You might want to set `keySeparator: false` and `namespaceSeparator: false`.
   // That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
-  output: 'src/languages/test/$LOCALE.json',
+  output: 'src/languages/$LOCALE.json',
   // Supports $LOCALE and $NAMESPACE injection
   // Supports JSON (.json) and YAML (.yml) file formats
   // Where to write the locale files relative to process.cwd()
   input: ['src/**/*.{js,jsx}'],
   // An array of globs that describe where to look for source files
   // relative to the location of the configuration file
-  sort: true,
+  sort: false,
   // Whether or not to sort the catalog
   skipDefaultValues: false,
   // Whether to ignore default values.
