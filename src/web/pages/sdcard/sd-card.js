596fbd5820eddb3a688d95e5cda3dc2dfb315130
kevin.kao <Kevin.Kao@androvideo.com>
2021-01-13 11:34:01 +0800

    call sd recording api with current settings when enabling sd card

--- src/web/pages/sdcard/sd-card.js
+++ src/web/pages/sdcard/sd-card.js
@@ -57,6 +57,7 @@ const SDCard = ({
     }
 
     if (!prevValues.sdEnabled && nextValues.sdEnabled) {
+      onSubmit(nextValues);
       return callApi('enableSD', {sdEnabled: nextValues.sdEnabled});
     }
 
